47a0b6b3f041345aebb5dae4269175b11f4f63de
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2022-12-19 14:08:31 +0100

    public constants, little text

--- libraries/JGUZDV.AspNetCore.DataProtection/src/Constants.cs
+++ libraries/JGUZDV.AspNetCore.DataProtection/src/Constants.cs
@@ -1,6 +1,6 @@
 namespace JGUZDV.AspNetCore.DataProtection;
 
-internal static class Constants
+public static class Constants
 {
     public const string DefaultSectionName = "JGUZDV:DataProtection";
 }
