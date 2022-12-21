3d1aa07526ad3a0a6463969cf62d8c73d3df2927
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2022-12-21 11:45:36 +0100

    Add readme

--- libraries/JGUZDV.AspNetCore.DataProtection/src/DataProtectionExtensions.cs
+++ libraries/JGUZDV.AspNetCore.DataProtection/src/DataProtectionExtensions.cs
@@ -8,18 +8,12 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DataProtectionExtensions
 {
-    public static IDataProtectionBuilder AddZDVDataProtectionConfig(this IServiceCollection services, IConfigurationSection configurationSection, IWebHostEnvironment? environment = null)
-    {
-        return services.AddDataProtection().UseDataProtectionConfig(configurationSection, environment);
-    }
+    public static IDataProtectionBuilder AddZDVDataProtection(this IServiceCollection services, IConfigurationSection configurationSection, IWebHostEnvironment? environment = null) 
+        => services.AddDataProtection().UseDataProtectionConfig(configurationSection, environment);
 
-    public static IDataProtectionBuilder AddZDVDataProtectionConfig(this IServiceCollection services, IConfiguration config, IWebHostEnvironment? environment = null)
-    {
-        return services.AddZDVDataProtectionConfig(config.GetSection(Constants.DefaultSectionName), environment);
-    }
+    public static IDataProtectionBuilder AddZDVDataProtection(this IServiceCollection services, IConfiguration config, IWebHostEnvironment? environment = null) 
+        => services.AddZDVDataProtection(config.GetSection(Constants.DefaultSectionName), environment);
 
-    public static IDataProtectionBuilder AddZDVDataProtectionConfig(this WebApplicationBuilder builder)
-    {
-        return builder.Services.AddZDVDataProtectionConfig(builder.Configuration.GetSection(Constants.DefaultSectionName), builder.Environment);
-    }
+    public static IDataProtectionBuilder AddZDVDataProtection(this WebApplicationBuilder builder, string sectionName = Constants.DefaultSectionName) 
+        => builder.Services.AddZDVDataProtection(builder.Configuration.GetSection(sectionName), builder.Environment);
 }
