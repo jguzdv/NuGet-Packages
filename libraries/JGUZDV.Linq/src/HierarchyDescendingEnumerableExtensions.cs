55f83ad0696d4afcbf08a888a46d5c2e3784e782
Thomas Ottenhus <glatzert@uni-mainz.de>
2023-01-11 11:22:59 +0100

    Linq ext (#9)
    * Implement Ascendents, Descendents
    * Workflows
    
    Co-authored-by: Ottenhus, Thomas <ottenhus@uni-mainz.de>

---
+++ libraries/JGUZDV.Linq/src/HierarchyDescendingEnumerableExtensions.cs
@@ -0,0 +1,87 @@
+﻿namespace System.Linq;
+
+public static class HierarchyDescendingEnumerableExtensions
+{
+    public static IEnumerable<TSource> Descendants<TSource>(
+        this TSource startNode,
+        Func<TSource, IEnumerable<TSource>?> childrenSelector,
+        ChildEnumerationStrategy strategy = ChildEnumerationStrategy.DepthFirst
+        )
+        => Descendants(startNode, childrenSelector, strategy, false);
+
+    public static IEnumerable<TSource> DescendantsAndSelf<TSource>(
+        this TSource startNode,
+        Func<TSource, IEnumerable<TSource>?> childrenSelector,
+        ChildEnumerationStrategy strategy = ChildEnumerationStrategy.DepthFirst)
+        => Descendants(startNode, childrenSelector, strategy, true);
+
+
+    public static IEnumerable<TSource> Descendants<TSource>(
+        this TSource startNode,
+        Func<TSource, IEnumerable<TSource>?> childrenSelector,
+        ChildEnumerationStrategy strategy,
+        bool includeStartNode)
+        => strategy switch
+        {
+            ChildEnumerationStrategy.BreadthFirst => BreadthFirstDescendants(startNode, childrenSelector, includeStartNode),
+            ChildEnumerationStrategy.DepthFirst => DepthFirstDescendants(startNode, childrenSelector, includeStartNode),
+            _ => throw new NotSupportedException()
+        };
+
+
+
+    private static IEnumerable<TSource> BreadthFirstDescendants<TSource>(
+        this TSource startNode,
+        Func<TSource, IEnumerable<TSource>?> childrenSelector,
+        bool includeStartNode)
+    {
+        var result = new List<TSource>();
+        if (includeStartNode)
+            yield return startNode;
+
+        var queue = new Queue<TSource>();
+        EnqueueChildren(queue, startNode, childrenSelector);
+        
+        while(queue.Count > 0)
+        {
+            var child = queue.Dequeue();
+            yield return child;
+
+            EnqueueChildren(queue, child, childrenSelector);
+        }
+
+        static void EnqueueChildren(Queue<TSource> queue, TSource node, Func<TSource, IEnumerable<TSource>?> childrenSelector)
+        {
+            foreach (var child in node.Children(childrenSelector))
+                queue.Enqueue(child);
+        }
+    }
+
+
+    private static IEnumerable<TSource> DepthFirstDescendants<TSource>(
+        this TSource startNode,
+        Func<TSource, IEnumerable<TSource>?> childrenSelector,
+        bool includeStartNode)
+    {
+        var result = Enumerable.Empty<TSource>();
+        if (includeStartNode)
+            yield return startNode;
+
+        var children = childrenSelector(startNode);
+        if (children == null)
+            yield break;
+
+        foreach (var child in children)
+        {
+            yield return child;
+            foreach (var grandChild in child.DepthFirstDescendants(childrenSelector, false))
+            { 
+                yield return grandChild; 
+            }
+        }
+    }
+
+
+    private static IEnumerable<TSource> Children<TSource>(this TSource node, Func<TSource, IEnumerable<TSource>?> childrenSelector)
+        => childrenSelector(node) ?? Enumerable.Empty<TSource>();
+}
---
+++ libraries/JGUZDV.Linq/src/OrderByExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace System.Linq;
+
+public static class OrderByExtensions
+{
+    public static IOrderedEnumerable<TSource> OrderBy<TSource, TKey>(
+        this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector,
+        OrderDirection direction
+    )
+    {
+        if (direction == OrderDirection.Ascending)
+            return source.OrderBy(keySelector);
+        else
+            return source.OrderByDescending(keySelector);
+    }
+
+    public static IOrderedEnumerable<TSource> ThenBy<TSource, TKey>(
+        this IOrderedEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector,
+        OrderDirection direction
+    )
+    {
+        if (direction == OrderDirection.Ascending)
+            return source.ThenBy(keySelector);
+        else
+            return source.ThenByDescending(keySelector);
+    }
+
+    public static OrderDirection Toggle(this OrderDirection s)
+        => s == OrderDirection.Ascending
+            ? OrderDirection.Descending
+            : OrderDirection.Ascending;
+}
+
+public enum OrderDirection
+{
+    Ascending,
+    Descending
+}
