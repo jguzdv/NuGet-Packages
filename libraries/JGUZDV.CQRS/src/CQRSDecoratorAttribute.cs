60230e06c575686dc26069f68f3ecf1d61e9bc7d
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-01-23 14:17:09 +0100

    DI-Helpers

---
+++ libraries/JGUZDV.CQRS/src/CQRSDecoratorAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace JGUZDV.CQRS
+{
+    /// <summary>
+    /// Flags a class as a "Decorator".
+    /// Decorators won't be automatically registered as ICommandHandler<> or IQueryHandler<> during DI setup.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class CQRSDecoratorAttribute : Attribute { }
+}
