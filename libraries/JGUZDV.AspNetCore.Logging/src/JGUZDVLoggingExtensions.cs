8ee712a9fda1ed48ac5819f06383daf94473fe7d
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-01-25 12:13:38 +0100

    Odd

--- libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
@@ -1,13 +1,5 @@
 ﻿using JGUZDV.AspNetCore.Logging;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Compact;
-using System.Runtime.InteropServices;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -15,8 +7,6 @@ public static class JGUZDVLoggingExtensions
 {
     public static WebApplicationBuilder UseJGUZDVLogging(this WebApplicationBuilder builder, string configSectionName = Constants.DefaultSectionName)
     {
-        builder.WebHost.UseSerilog((WebHostBuilderContext ctx, abc) => { })
-
         builder.Host.UseJGUZDVLogging(configSectionName);
         return builder;
     }
