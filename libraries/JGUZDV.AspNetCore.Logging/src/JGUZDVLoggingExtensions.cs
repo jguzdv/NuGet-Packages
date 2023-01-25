49f7f8a271e1d9a94d5d8d17d767f9111967b475
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-01-25 11:57:03 +0100

    Reuse JGUZDV.Extensions.Logging

---
+++ libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using JGUZDV.AspNetCore.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class JGUZDVLoggingExtensions
+{
+    public static WebApplicationBuilder UseJGUZDVLogging(this WebApplicationBuilder builder, string configSectionName = Constants.DefaultSectionName)
+    {
+        builder.WebHost.UseSerilog((WebHostBuilderContext ctx, abc) => { })
+
+        builder.Host.UseJGUZDVLogging(configSectionName);
+        return builder;
+    }
+}
+
