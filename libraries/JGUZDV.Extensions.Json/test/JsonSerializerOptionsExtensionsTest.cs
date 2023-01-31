00940ef1f47fb7a0f11861cb03e161feed5958c7
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-01-31 11:54:40 +0100

    OptionExtensions Test

---
+++ libraries/JGUZDV.Extensions.Json/test/JsonSerializerOptionsExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace JGUZDV.Extensions.Json.Tests
+{
+    public class JsonSerializerOptionsExtensionsTest
+    {
+        [Fact]
+        public void Registeres_all_Converters()
+        {
+            var sut = new JsonSerializerOptions();
+            sut.AddZDVDefaults();
+
+            Assert.Null(sut.PropertyNamingPolicy);
+            Assert.Null(sut.DictionaryKeyPolicy);
+        }
+    }
+}
