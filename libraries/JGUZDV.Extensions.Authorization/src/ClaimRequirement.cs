177064156d58bdc0d440a543de433d123b0c9181
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-03-14 17:12:58 +0100

    WIP

---
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirement.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace JGUZDV.Extensions.Authorization;
+
+[JsonDerivedType(typeof(ClaimValueRequirement), typeDiscriminator: "Value")]
+[JsonDerivedType(typeof(ClaimRequirements), typeDiscriminator: "List")]
+public abstract class ClaimRequirement
+{
+    public abstract bool SatisfiesRequirement(ClaimsPrincipal principal);
+}
---
+++ libraries/JGUZDV.Extensions.Authorization/test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace JGUZDV.Extensions.Authorization.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
