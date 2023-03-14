e91e5313efa4d38e6740496fe48639c9f3c98b77
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-03-14 17:12:58 +0100

    WIP

---
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirementJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JGUZDV.Extensions.Authorization
+{
+    public class ClaimRequirementJsonConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeof(ClaimRequirement).IsAssignableFrom(typeToConvert);
+        }
+
+        public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirements.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace JGUZDV.Extensions.Authorization;
+
+public sealed class ClaimRequirements : ClaimRequirement
+{
+    [JsonConstructor]
+    public ClaimRequirements(List<ClaimRequirement> requirements, ClaimRequirementListMatchType matchType)
+    {
+        Requirements = requirements;
+        MatchType = matchType;
+    }
+
+    public List<ClaimRequirement> Requirements { get; }
+    public ClaimRequirementListMatchType MatchType { get; }
+
+
+    public sealed override bool SatisfiesRequirement(ClaimsPrincipal principal)
+        => MatchType switch
+        {
+            ClaimRequirementListMatchType.MatchAll => Requirements.All(r => r.SatisfiesRequirement(principal)),
+            ClaimRequirementListMatchType.MatchAny => Requirements.Any(r => r.SatisfiesRequirement(principal)),
+            _ => false
+        };
+}
+
+public enum ClaimRequirementListMatchType
+{
+    MatchAll,
+    MatchAny
+}
