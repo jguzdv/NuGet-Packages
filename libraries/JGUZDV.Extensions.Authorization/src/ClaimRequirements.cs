e0be4da06af35088dd4085c262957222a61ff8ea
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-03-20 10:30:43 +0100

    Tests

--- libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirements.cs
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirements.cs
@@ -12,6 +12,12 @@ public sealed class ClaimRequirementCollection : ClaimRequirement
         MatchType = matchType;
     }
 
+    public ClaimRequirementCollection(ClaimRequirementListMatchType matchType, params ClaimRequirement[] requirements)
+    {
+        Requirements = requirements.ToList();
+        MatchType = matchType;
+    }
+
     public List<ClaimRequirement> Requirements { get; }
     public ClaimRequirementListMatchType MatchType { get; }
 
@@ -19,7 +25,7 @@ public sealed class ClaimRequirementCollection : ClaimRequirement
     public sealed override bool SatisfiesRequirement(ClaimsPrincipal principal)
         => MatchType switch
         {
-            ClaimRequirementListMatchType.MatchAll => Requirements.All(r => r.SatisfiesRequirement(principal)),
+            ClaimRequirementListMatchType.MatchAll => Requirements.Any() && Requirements.All(r => r.SatisfiesRequirement(principal)),
             ClaimRequirementListMatchType.MatchAny => Requirements.Any(r => r.SatisfiesRequirement(principal)),
             _ => false
         };
--- libraries/JGUZDV.Extensions.Authorization/test/ClaimValueRequirementTests.cs
+++ libraries/JGUZDV.Extensions.Authorization/test/ClaimValueRequirementTests.cs
@@ -17,17 +17,26 @@ public class ClaimValueRequirementTests
     }
 
     [Fact]
-    public void Matching_Claim_Can_Satisfy_ClaimValueRequirement()
+    public void Requirement_can_be_satisfied()
     {
         var sut = new ClaimValueRequirement("sub", "subject");
         var result = sut.SatisfiesRequirement(_testUser);
 
         Assert.True(result);
     }
+    
+    [Fact]
+    public void Requirement_can_fail()
+    {
+        var sut = new ClaimValueRequirement("sub", "false");
+        var result = sut.SatisfiesRequirement(_testUser);
+
+        Assert.False(result);
+    }
 
     [Fact]
-    public void TypeMatch_Is_CI()
-    {
+    public void Requirement_Type_Is_CI()
+    { 
         var sut = new ClaimValueRequirement("SUB", "subject");
         var result = sut.SatisfiesRequirement(_testUser);
 
@@ -35,16 +44,34 @@ public class ClaimValueRequirementTests
     }
 
     [Fact]
-    public void ValueMatch_Is_CS()
+    public void Requirement_Type_StringComparer_Can_Be_Changed()
     {
-        var sut = new ClaimValueRequirement("sub", "suBjEcT");
+        var sut = new ClaimValueRequirement("SUB", "subject", claimTypeComparison: StringComparison.InvariantCulture);
         var result = sut.SatisfiesRequirement(_testUser);
 
         Assert.False(result);
     }
 
     [Fact]
-    public void Wildcard_Type_Can_Satisfy_ClaimValueRequirement()
+    public void Requirement_Value_Is_CS()
+    {
+        var sut = new ClaimValueRequirement("sub", "suBjEcT");
+        var result = sut.SatisfiesRequirement(_testUser);
+
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void Requirement_Value_StringComparer_Can_Be_Changed()
+    {
+        var sut = new ClaimValueRequirement("sub", "suBjEcT", claimValueComparison: StringComparison.OrdinalIgnoreCase);
+        var result = sut.SatisfiesRequirement(_testUser);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Requirement_With_Wildcard_Type()
     {
         var sut = new ClaimValueRequirement("*", "subject");
         var result = sut.SatisfiesRequirement(_testUser);
@@ -53,7 +80,7 @@ public class ClaimValueRequirementTests
     }
     
     [Fact]
-    public void Wildcard_Value_Can_Satisfy_ClaimValueRequirement()
+    public void Requirement_With_Wildcard_Value()
     {
         var sut = new ClaimValueRequirement("sub", "*");
         var result = sut.SatisfiesRequirement(_testUser);
@@ -62,9 +89,18 @@ public class ClaimValueRequirementTests
     }
 
     [Fact]
-    public void NonMatching_Claim_Cannot_Satisfy_ClaimValueRequirement()
+    public void Requirement_With_Wildcard_Type_But_Disabled_Wildcard()
     {
-        var sut = new ClaimValueRequirement("sub", "will-not-match");
+        var sut = new ClaimValueRequirement("*", "subject", disableWildcardMatch: true);
+        var result = sut.SatisfiesRequirement(_testUser);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Requirement_With_Wildcard_Value_But_Disabled_Wildcard()
+    {
+        var sut = new ClaimValueRequirement("sub", "*", disableWildcardMatch: true);
         var result = sut.SatisfiesRequirement(_testUser);
 
         Assert.False(result);
