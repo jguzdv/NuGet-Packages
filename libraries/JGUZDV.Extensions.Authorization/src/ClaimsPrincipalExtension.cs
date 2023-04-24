ee6dd78ec0417eab8605a619375bd369ec6901ee
Lunau, Marcus <lunaum@uni-mainz.de>
2023-04-24 11:46:43 +0200

    added NullRequirement StaisfiesRequirement => IsStisfiedBy

---
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimsPrincipalExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGUZDV.Extensions.Authorization
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static bool SatisfiesRequirement(this ClaimsPrincipal currentUser, ClaimRequirement requirement)
+        {
+            return requirement.IsSatisfiedBy(currentUser);
+        }
+    }
+}
---
+++ libraries/JGUZDV.Extensions.Authorization/src/NullRequirement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGUZDV.Extensions.Authorization
+{
+    public class NullRequirement : ClaimRequirement
+    {
+        public override bool IsSatisfiedBy(ClaimsPrincipal principal)
+        {
+            return false;
+        }
+    }
+}
