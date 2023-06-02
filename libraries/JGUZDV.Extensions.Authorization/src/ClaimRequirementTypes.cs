522fb1f590aeaa7f21c30d9a423dedad41d26b52
Lunau, Marcus <lunaum@uni-mainz.de>
2023-06-02 13:35:38 +0200

    Enum Hinzugefügt

---
+++ libraries/JGUZDV.Extensions.Authorization/src/ClaimRequirementTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGUZDV.Extensions.Authorization
+{
+    public enum ClaimRequirementType
+    {
+        ClaimValueRequirement,
+        ClaimRequirementCollection
+    }
+}
