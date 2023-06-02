819a9f45863d03bea22f0fb7e6e850b99d424b20
Thomas Ottenhus <glatzert@uni-mainz.de>
2023-06-02 13:38:48 +0200

    Json extensions (#35)
    ---------
    
    Co-authored-by: Ottenhus, Thomas <ottenhus@uni-mainz.de>

---
+++ libraries/JGUZDV.Extensions.Json/src/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,32 @@
+﻿using JGUZDV.Extensions.Json.Converters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JGUZDV.Extensions.Json;
+
+public static class JsonSerializerOptionsExtensions
+{
+    /// <summary>
+    /// This will set some default behaviour to JsonSerializerOptions:
+#if NET6_0
+    /// - Add DateOnlyConverter
+    /// - Add TimeOnlyConverter
+#endif
+    /// - Add StringTrimmingJsonConverter
+    /// - Set DefaultIgnoreCondition to "WhenWritingNull"
+    /// - PropertyNamingPolicy to 'null'
+    /// - DictonaryKeyPolicy to 'null'
+    /// </summary>
+    public static void SetJGUZDVDefaults(this JsonSerializerOptions opt)
+    {
+#if NET6_0
+        opt.Converters.Add(new DateOnlyConverter());
+        opt.Converters.Add(new TimeOnlyConverter());
+#endif
+        opt.Converters.Add(new StringTrimmingJsonConverter());
+
+        opt.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        opt.PropertyNamingPolicy = null;
+        opt.DictionaryKeyPolicy = null;
+    }
+}
---
+++ libraries/JGUZDV.Extensions.Json/test/TimeOnlyConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using JGUZDV.Extensions.Json.Converters;
+using System.Text.Json;
+
+namespace JGUZDV.Extensions.Json.Tests;
+
+public class TimeOnlyConverterTest
+{
+    [Fact]
+    public void TimeOnly_will_roundtrip_correctly() 
+    {
+        var opt = new JsonSerializerOptions();
+        opt.Converters.Add(new TimeOnlyConverter());
+
+        var original = new TimeOnly(23, 25, 25);
+        var json = JsonSerializer.Serialize(original, opt);
+
+        var actual = JsonSerializer.Deserialize<TimeOnly>(json, opt);
+        Assert.Equal(original, actual);
+    }
+}
