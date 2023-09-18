72deb23d4db9aafc9fe9a78d906946d1b45b4f6c
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-09-18 16:07:14 +0200

    Equality Tests

---
+++ libraries/JGUZDV.Extensions.Authorization/test/EqualityTests.cs
@@ -0,0 +1,62 @@
+﻿namespace JGUZDV.Extensions.Authorization.Tests
+{
+    public class EqualityTests
+    {
+        public class EqualityTestDescription
+        {
+            public EqualityTestDescription(bool shouldBeEqual)
+            {
+                ShouldBeEqual = shouldBeEqual;
+            }
+
+            public required ClaimRequirement CompareMe { get; init; }
+            public required ClaimRequirement? CompareWith { get; init; }
+            
+            public bool ShouldBeEqual { get; }
+        }
+
+        public static TheoryData<EqualityTestDescription> TestData => new()
+        {
+            new EqualityTestDescription(false)
+            {
+                CompareMe = new NullRequirement(),
+                CompareWith = new ClaimValueRequirement("t","v")
+            },
+            new EqualityTestDescription(false)
+            {
+                CompareMe = new NullRequirement(),
+                CompareWith = new ClaimRequirementCollection(RequirementCollectionMatchType.MatchAny)
+            },
+
+            new EqualityTestDescription(true)
+            {
+                CompareMe = new ClaimValueRequirement("type","value"),
+                CompareWith = new ClaimValueRequirement("type","value")
+            },
+            new EqualityTestDescription(true)
+            {
+                CompareMe = new ClaimRequirementCollection(RequirementCollectionMatchType.MatchAll, new ClaimValueRequirement("type","value"), new ClaimValueRequirement("type_1","value_!")),
+                CompareWith = new ClaimRequirementCollection(RequirementCollectionMatchType.MatchAll, new ClaimValueRequirement("type","value"), new ClaimValueRequirement("type_1","value_!")),
+            },
+
+            new EqualityTestDescription(false)
+            {
+                CompareMe = new ClaimValueRequirement("type","value",true),
+                CompareWith = new ClaimValueRequirement("type","value",false)
+            },
+            new EqualityTestDescription(false)
+            {
+                CompareMe = new ClaimRequirementCollection(RequirementCollectionMatchType.MatchAll, new ClaimValueRequirement("type","value"), new ClaimValueRequirement("type_1","value_!")),
+                CompareWith = new ClaimRequirementCollection(RequirementCollectionMatchType.MatchAll, new ClaimValueRequirement("type_1","value_1"), new ClaimValueRequirement("type","value")),
+            },
+
+
+        };
+
+        [Theory, MemberData(nameof(TestData))]
+        public void ExamTests(EqualityTestDescription testDescription)
+        {
+            Assert.Equal(testDescription.ShouldBeEqual, testDescription.CompareMe.Equals(testDescription.CompareWith));
+        }
+    }
+}
