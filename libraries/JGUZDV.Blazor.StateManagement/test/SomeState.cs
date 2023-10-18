62024ab7dbe3c02f0e6eb0378792614dc142fe4d
aaron-puhl <aaron_puhl@web.de>
2023-10-18 16:05:13 +0200

    Blazor state (#50)
    Blazor state management library
    ---------
    
    Co-authored-by: Aaron Puhl <aaropuhl@outlook.de>

---
+++ libraries/JGUZDV.Blazor.StateManagement/test/SomeState.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace JGUZDV.Blazor.StateManagement.Tests
+{
+    internal partial class SomeState : ObservableObject
+    {
+        [ObservableProperty]
+        private int _propertyOne;
+
+        [ObservableProperty]
+        private int _propertyTwo;
+
+        [ObservableProperty]
+        private int _propertyThree;
+    }
+}
