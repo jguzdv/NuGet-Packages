58424f9c1d0db9a180f9bc59260cbd695db6cd78
Thomas Ottenhus <glatzert@uni-mainz.de>
2023-11-22 17:25:02 +0100

    OpenIddict automatic key rollover (#62)
    * Implement OpenIdDict KeyManagement
    * Build pipeline
    * Empty test project
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <ottenhus@uni-mainz.de>

---
+++ libraries/JGUZDV.OpenIddict.KeyManager/src/KeyManagerOptionsValidator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace JGUZDV.OpenIddict.KeyManager;
+
+public class KeyManagerOptionsValidator : IValidateOptions<KeyManagerOptions>
+{
+    public ValidateOptionsResult Validate(string name, KeyManagerOptions options)
+    {
+        if (string.IsNullOrEmpty(options.KeyStorePath))
+            return ValidateOptionsResult.Fail($"{nameof(options.KeyStorePath)} was empty");
+
+        return ValidateOptionsResult.Success;
+    }
+}
