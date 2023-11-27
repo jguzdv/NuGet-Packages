107b521993df632fb96c63bde1526c5941a675a5
Aaron Puhl <aaron_puhl@web.de>
2023-11-27 14:36:51 +0100

    add default implementations and extensions

---
+++ JGUZDV.ClientStorage.Extensions/ClientStorageExtensions.cs
@@ -0,0 +1,38 @@
+﻿
+using JGUZDV.ClientStorage;
+using JGUZDV.ClientStorage.Extensions;
+using JGUZDV.ClientStorage.Store;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// ServiceCollection extensions for ClientStorage package.
+/// </summary>
+public static class ClientStorageExtensions
+{
+    /// <summary>
+    /// Adds <see cref="ClientStore"/> without client side persistence.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddClientStoreWithNullStorage(this IServiceCollection services)
+    {
+        services.AddSingleton<IKeyValueStorage, NullStorage>();
+        services.AddSingleton<ILifeCycleEvents, LifeCycleEvents>();
+        services.AddSingleton<ClientStore>();
+        return services;
+    }
+
+    /// <summary>
+    /// Adds <see cref="ClientStore"/> with localStorage persistence
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddClientStoreWASMDefaults(this IServiceCollection services)
+    {
+        services.AddSingleton<IKeyValueStorage, LocalStorage>();
+        services.AddSingleton<ILifeCycleEvents, LifeCycleEvents>();
+        services.AddSingleton<ClientStore>();
+        return services;
+    }
+}
---
+++ JGUZDV.ClientStorage.Extensions/LifeCycleEvents.cs
@@ -0,0 +1,30 @@
+﻿namespace JGUZDV.ClientStorage.Extensions;
+
+/// <summary>
+/// Simple implementation for ILifeCycleEvents <see cref="Stopped"/> and <see cref="Resumed"/> 
+/// must be triggered by the consumer using <see cref="TriggerStopped(object?, EventArgs)"/> 
+/// and <see cref="TriggerResumed(object?, EventArgs)"/>
+/// </summary>
+public class LifeCycleEvents : ILifeCycleEvents
+{
+
+    /// <summary>
+    /// Trigger the stopped event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public void TriggerStopped(object? sender, EventArgs args) => Stopped?.Invoke(sender, args);
+
+    /// <summary>
+    /// Trigger the resumed event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public void TriggerResumed(object? sender, EventArgs args) => Resumed?.Invoke(sender, args);
+
+    /// <inheritdoc />
+    public event EventHandler? Stopped;
+
+    /// <inheritdoc />
+    public event EventHandler? Resumed;
+}
