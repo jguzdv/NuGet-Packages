820c1acceda99a763db4efb5c001195b03df16e8
aysiebel <147617497+aysiebel@users.noreply.github.com>
2023-12-01 11:15:08 +0100

    Modal with dialog tag (#64)
    * Changed Modal to us `<dialog>` instead of a top-level container
    * Removed `ModalService` and `<ModalContainer>`

---
+++ libraries/JGUZDV.Blazor.Components/src/wwwroot/js/Modal.js
@@ -0,0 +1,9 @@
+﻿export function OpenDialog(guid) {
+    var dialog = document.getElementById(guid);
+    dialog.showModal();
+};
+
+export function CloseDialog(guid) {
+    var dialog = document.getElementById(guid);
+    dialog.close();
+};
