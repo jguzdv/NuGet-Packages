9f7138c0133e41d5ddf5a4523c0816534dca9636
Thomas Ottenhus <glatzert@uni-mainz.de>
2024-01-12 15:42:03 +0100

    Blazor AuthenticationStateProvider (#69)
    * AuthenticationStateProvider für Blazor WASM
    * Update .csproj (Bump Version)
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <ottenhus@uni-mainz.de>

---
+++ libraries/JGUZDV.Blazor.Components/src/Authentication/DelegatedAuthenticationStateProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace JGUZDV.Blazor.Components.Authentication;
+
+public static class DelegatedAuthenticationStateProviderExtensions
+{
+    public static IServiceCollection AddAuthenticationStateProvider<TFetchAuthenticationState>(
+        this IServiceCollection services)
+        where TFetchAuthenticationState : class, IFetchAuthenticationState
+    {
+        services.TryAddSingleton(sp => TimeProvider.System);
+        services.TryAddScoped<IFetchAuthenticationState, TFetchAuthenticationState>();
+        
+        services.TryAddSingleton<DelegatedAuthenticationStateProvider>();
+
+        return services;
+    }
+}
