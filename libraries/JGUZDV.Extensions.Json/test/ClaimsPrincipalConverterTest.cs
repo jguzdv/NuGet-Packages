f0cc7ef35d13b9b1aa917a3f86b72edacdfb0c81
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2024-01-16 15:29:00 +0100

    Add ClaimsPrincipalConverter

---
+++ libraries/JGUZDV.Extensions.Json/test/ClaimsPrincipalConverterTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+using JGUZDV.Extensions.Json.Converters;
+
+namespace JGUZDV.Extensions.Json.Tests
+{
+    public class ClaimsPrincipalConverterTest
+    {
+        [Fact]
+        public void ClaimsPrincipal_will_roundtrip_correctly()
+        {
+            var opt = new JsonSerializerOptions();
+            opt.Converters.Add(new ClaimsPrincipalConverter());
+
+            var original = new ClaimsPrincipal(new[] {
+                new ClaimsIdentity(new[] {
+                    new Claim("t1", "v1"),
+                    new Claim("t2", "v2")
+                }, "test", "name", "role"),
+                new ClaimsIdentity(new[]
+                {
+                    new Claim("x1", "x2")
+                }, "test-amr", "displayName", "memberof")
+            });
+            var json = JsonSerializer.Serialize(original, opt);
+
+            var actual = JsonSerializer.Deserialize<ClaimsPrincipal>(json, opt);
+            Assert.Equal(2, actual.Identities.Count());
+        }
+    }
+}
