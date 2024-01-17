ba9f21e5cfd9b6a53dcb10fa6c0c07de9134aba8
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2024-01-17 17:13:38 +0100

    Add AppController

---
+++ libraries/JGUZDV.Blazor.WasmHost/src/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+
+using JGUZDV.Extensions.Json.Converters;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JGUZDV.Blazor.WasmHost.Controllers
+{
+    [Route("_app")]
+    public class AppController : ControllerBase
+    {
+        [HttpGet("sign-in")]
+        [Authorize]
+        public IActionResult SignInAsync(string nextUrl = "~/")
+        {
+            if (!nextUrl.StartsWith('~'))
+                nextUrl = '~' + nextUrl;
+
+            return LocalRedirect(nextUrl);
+        }
+
+        [HttpGet("principal")]
+        public IActionResult GetPrincipal()
+        {
+            if (User.Identities.Any(x => x.IsAuthenticated))
+            {
+                var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+                options.Converters.Add(new ClaimsPrincipalConverter());
+
+                var jsonPrincipal = JsonSerializer.Serialize(User, options);
+
+                return Ok(jsonPrincipal);
+            }
+
+            return Ok();
+        }
+
+
+        [HttpGet("sign-out")]
+        public async Task<IActionResult> SignOutAsync(string nextUrl = "~/")
+        {
+            if (!nextUrl.StartsWith('~'))
+                nextUrl = '~' + nextUrl;
+
+            await HttpContext.SignOutAsync();
+            return LocalRedirect(nextUrl);
+        }
+
+
+    }
+}
