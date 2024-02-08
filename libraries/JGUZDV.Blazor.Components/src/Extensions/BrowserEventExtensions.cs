e14faa9513f0b5185875c074515f022a4914e9eb
aaron-puhl <aaron_puhl@web.de>
2024-02-08 16:07:18 +0100

    Multi select updates (#80)
    fix keyboard usage
    add active item badges
    add close button (bottom)
    add icon fragments

---
+++ libraries/JGUZDV.Blazor.Components/src/Extensions/BrowserEventExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace JGUZDV.Blazor.Components.Extensions
+{
+    public static class BrowserEventExtensions
+    {
+        public static async Task OnEnter(this KeyboardEventArgs e, Func<Task> action)
+        {
+            if (e.Code == "Enter")
+            {
+                await action();
+            }
+        }
+
+        public static void OnEnter(this KeyboardEventArgs e, Action action)
+        {
+            if (e.Code == "Enter")
+            {
+                action();
+            }
+        }
+    }
+}
