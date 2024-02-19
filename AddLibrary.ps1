f8187e1f81f7c7b5a45a42e2840d58a247f19a34
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2024-02-19 15:13:46 +0100

    Test stub

--- AddLibrary.ps1
+++ AddLibrary.ps1
@@ -1,22 +1,19 @@
-[CmdLetBinding]
-function Add-JGUZDVLibrary {
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline=$true)]
-        [string] $LibraryName,
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline=$true)]
+    [string] $LibraryName,
 
-        [Parameter()]
-        [string] $Template = 'classlib'
-    )
+    [Parameter()]
+    [string] $Template = 'classlib'
+)
 
-    process {
-        Push-Location $PSScriptRoot
+process {
+    Push-Location $PSScriptRoot
 
-        Invoke-Expression "dotnet new $Template -n $LibraryName -o ./libraries/$LibraryName/src"
-        Invoke-Expression "dotnet new xunit -n $LibraryName.Tests -o ./libraries/$LibraryName/test"
+    Invoke-Expression "dotnet new $Template -n $LibraryName -o ./libraries/$LibraryName/src"
+    Invoke-Expression "dotnet new xunit -n $LibraryName.Tests -o ./libraries/$LibraryName/test"
 
-        Invoke-Expression "dotnet sln . add ./libraries/$LibraryName/src"
-        Invoke-Expression "dotnet sln . add ./libraries/$LibraryName/test"
+    Invoke-Expression "dotnet sln . add ./libraries/$LibraryName/src"
+    Invoke-Expression "dotnet sln . add ./libraries/$LibraryName/test"
 
-        Pop-Location
-    }
+    Pop-Location
 }
---
+++ libraries/JGUZDV.WebApiHost/test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace JGUZDV.WebApiHost.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
