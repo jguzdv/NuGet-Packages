3cd2f89c9599664459dab4a1375b280ab0aa2355
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2024-02-21 09:38:55 +0100

    XMLDoc error

--- libraries/JGUZDV.CQRS/src/CQRSDecoratorAttribute.cs
+++ libraries/JGUZDV.CQRS/src/CQRSDecoratorAttribute.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Flags a class as a "Decorator".
-    /// Decorators won't be automatically registered as ICommandHandler<> or IQueryHandler<> during DI setup.
+    /// Decorators won't be automatically registered as ICommandHandler&lt;&gt; or IQueryHandler&lt;&gt; during DI setup.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class CQRSDecoratorAttribute : Attribute { }
