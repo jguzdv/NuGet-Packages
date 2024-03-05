dc3cfe8bc6ca3e332490e0def56a7332baa73901
Aaron Puhl <aaropuhl@outlook.de>
2024-03-05 10:37:26 +0100

    clean dashboard state

--- libraries/JGUZDV.JobHost.Dashboard.Blazor/DashboardState.cs
+++ libraries/JGUZDV.JobHost.Dashboard.Blazor/DashboardState.cs
@@ -8,17 +8,12 @@ namespace JGUZDV.JobHost.Dashboard.Blazor
 {
     public partial class DashboardState : ObservableObject
     {
-        private Task<JobCollection> _jobs;
-        private ClientStore _clientStore;
+        private readonly ClientStore _clientStore;
+        private readonly IDashboardService _dashboardService;
 
-        public Task<JobCollection> Jobs 
-        { 
-            get => _jobs;
-            set
-            {
-                SetProperty(ref _jobs, value, nameof(Jobs));
-            }
-        }
+        [ObservableProperty]
+        private Task<JobCollection> _jobs;
+
         public DashboardState(IDashboardService dashboardService, ClientStore clientStore)
         {
             _dashboardService = dashboardService;
@@ -26,28 +21,25 @@ namespace JGUZDV.JobHost.Dashboard.Blazor
             Initialize();
         }
 
+        public Task ExecuteNow(int jobId)
+        {
+            return _dashboardService.ExecuteNow(jobId);
+        }
+
         private void Initialize()
         {
-           Jobs = _clientStore.GetOrLoad(
-                "SteveJobs",
-                new StoreOptions<JobCollection>
-                {
-                    LoadFunc = (ct) => _dashboardService.GetSteveJobs(),
-                    UsesBackgroundRefresh = true,
-                    ValueExpiry = TimeSpan.FromSeconds(15)
-                });
+            Jobs = _clientStore.GetOrLoad(
+                 "SteveJobs",
+                 new StoreOptions<JobCollection>
+                 {
+                     LoadFunc = (ct) => _dashboardService.GetSteveJobs(),
+                     UsesBackgroundRefresh = true,
+                     ValueExpiry = TimeSpan.FromSeconds(15)
+                 });
 
             _clientStore.ValueChanged +=
                  (e) =>
                 Jobs = _clientStore.Get<JobCollection>(e.Key);
-        }
-
-        private IDashboardService _dashboardService;
-
-
-        public Task ExecuteNow(int jobId)
-        {
-            return _dashboardService.ExecuteNow(jobId);
         }
     }
 }
