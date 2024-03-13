fca2475046a92169cee424bfcddfd560ae2c0399
Beckhoff, Tobias <tbeckhof@uni-mainz.de>
2024-03-13 16:04:56 +0100

    Added option writeToProviders to logging extensions.

--- libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
@@ -10,9 +10,11 @@ public static class JGUZDVLoggingExtensions
     /// <summary>
     /// Configures the web host to use the default logging configuration.
     /// </summary>
-    public static WebApplicationBuilder UseJGUZDVLogging(this WebApplicationBuilder builder, string configSectionName = JGUZDV.Extensions.Logging.Constants.DefaultSectionName)
+    public static WebApplicationBuilder UseJGUZDVLogging(this WebApplicationBuilder builder, 
+        string configSectionName = JGUZDV.Extensions.Logging.Constants.DefaultSectionName, 
+        bool writeToProviders = true)
     {
-        builder.Host.UseJGUZDVLogging(configSectionName);
+        builder.Host.UseJGUZDVLogging(configSectionName, writeToProviders);
         return builder;
     }
 }
