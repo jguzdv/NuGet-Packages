9dcfa69952cdf042e9e19c52cca68f7fdb295a95
fberwind <f.berwind@t-online.de>
2024-03-28 09:34:41 +0100

    now catches close event and closes the modal

--- libraries/JGUZDV.Blazor.Components/src/wwwroot/js/Modal.js
+++ libraries/JGUZDV.Blazor.Components/src/wwwroot/js/Modal.js
@@ -7,3 +7,12 @@ export function CloseDialog(guid) {
     var dialog = document.getElementById(guid);
     dialog.close();
 };
+
+export function EscapeListener(guid, reference) {
+    var dialog = document.getElementById(guid);
+    dialog.addEventListener("close", () => {
+        reference.invokeMethodAsync("Close");
+    })
+}
+
+
