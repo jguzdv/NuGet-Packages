c69bdd0945ab2cfe5d0cc41c44da699ae67c2dff
CodeSky004 <f.berwind@t-online.de>
2024-05-22 16:34:26 +0200

    removal of listener now via typescript

---
+++ libraries/JGUZDV.Blazor.Components/src/Typescript/Modal.ts
@@ -0,0 +1,37 @@
+﻿export class closeListener {
+    constructor() {
+        this.handleCloseEvent = this.handleCloseEvent.bind(this);
+    };
+
+    public addListener(guid, reference) {
+        var dialog = document.getElementById(guid);
+            dialog.addEventListener("close", this.handleCloseEvent(reference));
+    }
+
+    public removeListener(guid, reference) {
+        var dialog = document.getElementById(guid);
+            dialog.removeEventListener("close", this.handleCloseEvent(reference));
+        
+     }
+
+    private handleCloseEvent(reference) {
+       return () => reference.invokeMethodAsync("Close");
+    }
+}
+
+const testClass = new closeListener();
+
+export function OpenDialog(guid) {
+    var dialog = document.getElementById(guid) as HTMLDialogElement;
+    dialog.showModal();
+};
+
+export function CloseDialog(guid, reference) {
+    var dialog = document.getElementById(guid) as HTMLDialogElement;
+    testClass.removeListener(guid, reference);
+    dialog.close(); 
+};
+
+export function CloseListener(guid, reference) {
+    testClass.addListener(guid, reference);
+};
--- libraries/JGUZDV.Blazor.Components/src/wwwroot/js/Modal.js
+++ libraries/JGUZDV.Blazor.Components/src/wwwroot/js/Modal.js
@@ -1,15 +1,34 @@
-﻿export function OpenDialog(guid) {
+export class closeListener {
+    constructor() {
+        this.handleCloseEvent = this.handleCloseEvent.bind(this);
+    }
+    ;
+    addListener(guid, reference) {
+        var dialog = document.getElementById(guid);
+        dialog.addEventListener("close", this.handleCloseEvent(reference));
+    }
+    removeListener(guid, reference) {
+        var dialog = document.getElementById(guid);
+        dialog.removeEventListener("close", this.handleCloseEvent(reference));
+    }
+    handleCloseEvent(reference) {
+        return () => reference.invokeMethodAsync("Close");
+    }
+}
+const testClass = new closeListener();
+export function OpenDialog(guid) {
     var dialog = document.getElementById(guid);
     dialog.showModal();
-};
-
+}
+;
 export function CloseDialog(guid, reference) {
     var dialog = document.getElementById(guid);
-    dialog.removeEventListener("close", () => { reference.invokeMethodAsync("Close"); });
+    testClass.removeListener(guid, reference);
     dialog.close();
-};
-
+}
+;
 export function CloseListener(guid, reference) {
-    var dialog = document.getElementById(guid);
-    dialog.addEventListener("close", () => { reference.invokeMethodAsync("Close"); });
-    }
+    testClass.addListener(guid, reference);
+}
+;
+//# sourceMappingURL=Modal.js.map
