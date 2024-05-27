17cf9532cb4c18ffa0b517e4217b16ff13d515c4
Thomas Ottenhus <glatzert@uni-mainz.de>
2024-05-27 09:55:45 +0200

    Add check for allowed Scopes (#99)
    * RequiredScopes -> AllowedScopes
    * RequireScope Extensions method
    * Scopes need to be checked case-sensitive per oauth2
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

---
+++ libraries/JGUZDV.WebApiHost/src/Extensions/AuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace JGUZDV.WebApiHost.Extensions;
+
+/// <summary>
+/// Provides new Require* methods for AuthorizationPolicyBuilder
+/// </summary>
+public static class AuthorizationPolicyBuilderExtensions
+{
+    /// <summary>
+    /// Checks the list of scopes in the user's claims to see if any of them match the allowed scopes.
+    /// Since scopes can occure either one per claim or multiple scopes in a single claim, this method
+    /// splits the contents of the claim by spaces and checks if any of the resulting strings match any of the allowed scopes
+    /// </summary>
+    public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder builder, string allowedScope, string scopeClaimType = "scope")
+    {
+        return builder.RequireAssertion(context =>
+            context.User.FindAll(scopeClaimType).SelectMany(c => c.Value.Split(' '))
+                .Contains(allowedScope, StringComparer.Ordinal)
+        );
+    }
+
+    /// <summary>
+    /// Checks the list of scopes in the user's claims to see if any of them match the allowed scopes.
+    /// Since scopes can occure either one per claim or multiple scopes in a single claim, this method
+    /// splits the contents of the claim by spaces and checks if any of the resulting strings match any of the allowed scopes
+    /// </summary>
+    public static AuthorizationPolicyBuilder RequireAnyScope(this AuthorizationPolicyBuilder builder, IEnumerable<string> allowedScopes, string scopeClaimType = "scope")
+    {
+        return builder.RequireAssertion(context => 
+            context.User.FindAll(scopeClaimType).SelectMany(c => c.Value.Split(' '))
+                .Intersect(allowedScopes, StringComparer.Ordinal)
+                .Any()
+        );
+    }
+}
