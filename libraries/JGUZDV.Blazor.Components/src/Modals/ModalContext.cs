e09ef9fd387ce381e085020275fc8a02631c0f55
Ottenhus, Thomas <Ottenhus, Thomas>
2024-11-05 10:15:25 +0100

    Allow easier usage of ModalContext<T>

---
+++ libraries/JGUZDV.Blazor.Components/src/Modals/ModalContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace JGUZDV.Blazor.Components.Modals
+{
+    /// <summary>
+    /// Contains the Modal itself and the model that the modal is working with.
+    /// </summary>
+    public class ModalContext<TModel>
+        where TModel : class
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModalContext{TModel}"/> class.
+        /// </summary>
+        /// <param name="modal">The modal instance.</param>
+        /// <param name="model">The model instance.</param>
+        [SetsRequiredMembers]
+        public ModalContext(Modal<TModel> modal, TModel model)
+        {
+            Modal = modal;
+            Model = model;
+        }
+
+        /// <summary>
+        /// The modal instance.
+        /// </summary>
+        public Modal<TModel> Modal { get; }
+
+        /// <summary>
+        /// The model instance.
+        /// </summary>
+        public TModel Model { get; set; }
+    }
+}
