785f175970f7c46a5e5e90d1e5f3eaaa04369d67
Beckhoff, Tobias <tbeckhof@uni-mainz.de>
2024-11-13 16:37:10 +0100

    Added metrics options for asp net core apps. Re-arranged config process.

---
+++ libraries/JGUZDV.AspNetCore.Extensions.OpenTelemetry/src/AbstractJguZdvMeter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+
+namespace JGUZDV.AspNetCore.Extensions.OpenTelemetry
+{
+    /// <summary>
+    /// Inherit from this instance for JguZdv telemetry to create counts/histograms/gauges.
+    /// </summary>
+    public abstract class AbstractJguZdvMeter
+    {
+        /// <summary>
+        /// JguZdv OpenTelemetry options.
+        /// </summary>
+        protected readonly IOptions<AspNetCoreOpenTelemetryOptions> _options;
+
+        /// <summary>
+        /// The one and only meter instance that must be used to transmit values to Azure Monitor.
+        /// </summary>
+        protected readonly Meter _otMeter;
+
+
+        /// <summary>
+        /// Uses AspNetCoreOpenTelemetryOptions to create a default meter instance.
+        /// </summary>
+        /// <param name="options"></param>
+        protected AbstractJguZdvMeter(IOptions<AspNetCoreOpenTelemetryOptions> options)
+        {
+            _options = options;
+
+            if(_options.Value.UseMeter == null)
+            {
+                throw new ArgumentException("Can only be used if UseMeter is completly configured in appsettings.");
+            }
+
+            _otMeter = new Meter(_options.Value.UseMeter!.MeterName, options.Value.UseMeter!.MeterVersion);
+        }
+    }
+}
