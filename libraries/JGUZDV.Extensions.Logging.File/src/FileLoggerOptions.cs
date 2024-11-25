1fc00da0f7595a001687874176b95d525ec97533
Ottenhus, Thomas <Ottenhus, Thomas>
2024-11-25 11:54:31 +0100

    Adding options

--- libraries/JGUZDV.Extensions.Logging.File/src/FileLoggerOptions.cs
+++ libraries/JGUZDV.Extensions.Logging.File/src/FileLoggerOptions.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace JGUZDV.Extensions.Logging.File;
+﻿namespace JGUZDV.Extensions.Logging.File;
 
 /// <summary>
 /// Options for a <see cref="FileLogger"/>.
@@ -59,12 +57,65 @@ public class FileLoggerOptions
         }
     }
 
+    /// <summary>
+    /// Gets or sets the output directory, where log files will be written to.
+    /// </summary>
+    public string? OutputDirectory { get; set; }
 
-    /* TODO:
-     * Path
-     * Filename-Pattern
-     * Rollover by Size
-     * Rollover by Time
-     * Rollover by Date
-     */
+    /// <summary>
+    /// The discriminator may be included in the log-file name to e.g. distinguish between different machines.
+    /// </summary>
+    public string Discriminator { get; set; } = Environment.MachineName;
+
+    /// <summary>
+    /// The date format used in the log-file name. If you add hours or minutes here, the file will roll-over on every change.
+    /// </summary>
+    /// <value>
+    /// The default value is yyyy-MM-dd.
+    /// </value>
+    public string DateFormat { get; set; } = "yyyy-MM-dd";
+
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to use local time.
+    /// </summary>
+    /// <value>
+    /// The default value is false
+    /// </value>
+    public bool PreferLocalTime { get; set; }
+
+
+    /// <summary>
+    /// The format used to distinguish rolling log files.
+    /// </summary>
+    /// <value>
+    /// The default value is _000
+    /// </value>
+    public string RollingFileFormat { get; set; } = "_000";
+
+    /// <summary>
+    /// If set to <see langword="true" />, the rolling number will always be included in the log file name.
+    /// </summary>
+    /// <value>
+    /// The default is false.
+    /// </value>
+    public bool AlwaysIncludeRollingNumber = false;
+
+    /// <summary>
+    /// The maximum acceptable file size before rolling the log file in byte.
+    /// </summary>
+    /// <value>
+    /// The default value is 50Mb.
+    /// </value>
+    public long RollingFileSize { get; set; } = 50 * 1000 * 1024;
+
+
+    /// <summary>
+    /// Sets the filename pattern used to create the filenames.
+    /// </summary>
+    /// <remarks>
+    /// Use {Date} {Discriminator} and {Rolling} to include the date, discriminator and rolling number in the filename.
+    /// Do not append the File-Extension as it will be appended automatically.
+    /// </remarks>
+    public string FilenamePattern { get; set; } = "{Date}_{Discriminator}{Rolling}";
 }
