7890d2784324103ffc0c5249f9ff5337847f07c8
Ottenhus, Thomas <Ottenhus, Thomas>
2024-12-09 12:05:53 +0100

    AppHeader with Auth-Button

--- libraries/JGUZDV.Blazor.Components/samples/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Program.cs
@@ -1,8 +1,12 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Security.Claims;
+
 using JGUZDV.Blazor.Components;
 using JGUZDV.Blazor.Components.L10n;
 using JGUZDV.Blazor.Components.Samples;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,6 +19,30 @@ builder.Services.AddSupportedCultures(new List<string>() { "de", "en" });
 builder.Services.AddLocalization(["de", "en", "fr"]);
 builder.Services.AddBrowserClientStore();
 
+builder.Services.AddSingleton<AuthenticationStateProvider, SampleAuthenticationStateProvider>();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+
 var app = builder.Build();
 
 await app.RunAsync();
+
+
+internal class SampleAuthenticationStateProvider : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        return Task.FromResult(
+            new AuthenticationState(
+                new ClaimsPrincipal(
+                    new ClaimsIdentity(new List<Claim>
+                    {
+                        new("name", "Mustermann, Max")
+                    },
+                    "SampleAuth", "name", "role")
+                )
+            )
+        );
+    }
+}
