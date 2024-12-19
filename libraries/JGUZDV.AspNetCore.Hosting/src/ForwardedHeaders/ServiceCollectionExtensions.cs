b50564d1e6ebeb6d1c3c8d58cf4d24da16a116e6
Ottenhus, Thomas <Ottenhus, Thomas>
2024-12-19 16:50:58 +0100

    Docs

--- libraries/JGUZDV.AspNetCore.Hosting/src/ForwardedHeaders/ServiceCollectionExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Hosting/src/ForwardedHeaders/ServiceCollectionExtensions.cs
@@ -1,13 +1,20 @@
 ﻿using JGUZDV.AspNetCore.Hosting.Extensions;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace JGUZDV.AspNetCore.Hosting.ForwardedHeaders;
 
+/// <summary>
+/// Extension methods for the ServiceCollection.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Configures the ForwardedHeaders middleware.
+    /// </summary>
     public static WebApplicationBuilder ConfigureForwardedHeaders(
         this WebApplicationBuilder builder,
         Action<ForwardedHeadersOptions>? options = null,
@@ -19,6 +26,9 @@ public static class ServiceCollectionExtensions
         return builder;
     }
 
+    /// <summary>
+    /// Configures the ForwardedHeaders middleware.
+    /// </summary>
     public static IServiceCollection ConfigureForwardedHeaders(
         this IServiceCollection services,
         IConfiguration configSection,
@@ -27,6 +37,27 @@ public static class ServiceCollectionExtensions
         services.Configure<ForwardedHeadersOptions>(opt =>
         {
             configSection.Bind(opt);
+
+            var knownNetworks = configSection.GetSection("KnownNetworks").Get<string[]>();
+            if (knownNetworks != null)
+            {
+                opt.KnownNetworks.Clear();
+
+                foreach (var network in knownNetworks) {
+                    opt.KnownNetworks.Add(IPNetwork.Parse(network));
+                }
+            }
+
+            var knownProxies = configSection.GetSection("KnownProxies").Get<string[]>();
+            if (knownProxies != null)
+            {
+                opt.KnownProxies.Clear();
+                foreach (var proxy in knownProxies)
+                {
+                    opt.KnownProxies.Add(System.Net.IPAddress.Parse(proxy));
+                }
+            }
+
             configure?.Invoke(opt);
         });
 
