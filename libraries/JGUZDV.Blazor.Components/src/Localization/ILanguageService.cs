cf4e0e56182638c149b25c2f42f5a757cd89b951
Thomas Ottenhus <glatzert@uni-mainz.de>
2025-01-02 12:25:37 +0100

    Net core/hosting (#127)
    * Use @v4 and .net 9
    * Add JGUZDV.AspNetCore.Hosting
    * Add JGUZDV.Blazor.Hosting
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

---
+++ libraries/JGUZDV.Blazor.Components/src/Localization/ILanguageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGUZDV.Blazor.Components.Localization
+{
+    /// <summary>
+    /// Represents a service that provides language information
+    /// </summary>
+    public interface ILanguageService
+    {
+        /// <summary>
+        /// Initializes the service - call this before all other methods
+        /// </summary>
+        public Task InitializeService();
+
+        /// <summary>
+        /// Gets the current culture, e.g. de-DE or de
+        /// </summary>
+        public string GetCurrentCulture();
+
+        /// <summary>
+        /// Gets the current UI culture, e.g. de-DE or de
+        /// </summary>
+        public string GetCurrentUICulture();
+
+        /// <summary>
+        /// Get available languages for a language select.
+        /// </summary>
+        public List<LanguageItem>? GetLanguages();
+    }
+}
