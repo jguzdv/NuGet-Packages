d9c1bf5ca82c59b0ab72cf7182c67b960dca5ca6
Thomas Ottenhus <glatzert@uni-mainz.de>
2025-02-05 14:48:32 +0100

    Blazor components/cropper (#137)
    * Colocated ts / js
    
    * Add cropper to the package
    
    * Use Auto-Property for Parameter
    
    * Set rendermode
    
    * Nuget Specifics
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

---
+++ libraries/JGUZDV.Blazor.Components/src/Cropper/ImageTransformation.cs
@@ -0,0 +1,27 @@
+﻿namespace JGUZDV.Blazor.Components.Cropper;
+
+/// <summary>
+/// Represents the transformation of an image.
+/// </summary>
+public class ImageTransformation
+{
+    /// <summary>
+    /// The scale of the image. Default is 1.
+    /// </summary>
+    public int Scale { get; set; } = 1;
+
+    /// <summary>
+    /// The rotation of the image in degree. Default is 0.
+    /// </summary>
+    public int Rotate { get; set; } = 0;
+
+    /// <summary>
+    /// The horizontal flip of the image. Default is false.
+    /// </summary>
+    public bool FlipHorizontal { get; set; }
+
+    /// <summary>
+    /// The vertical flip of the image. Default is false.
+    /// </summary>
+    public bool FlipVertical { get; set; }
+}
---
+++ libraries/JGUZDV.Blazor.Components/src/Cropper/Size.cs
@@ -0,0 +1,8 @@
+﻿namespace JGUZDV.Blazor.Components.Cropper;
+
+/// <summary>
+/// Represents a size with height and width.
+/// </summary>
+/// <param name="Height">The height</param>
+/// <param name="Width">The width</param>
+public record Size(double Height, double Width);
