2529819de8bff8a9ab324333b964cb88c6cb1c9d
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-13 16:10:34 +0100

    Add loading indicator

--- libraries/JGUZDV.Blazor.Components/src/wwwroot/JGUZDV.Blazor.Components.lib.module.ts
+++ libraries/JGUZDV.Blazor.Components/src/wwwroot/JGUZDV.Blazor.Components.lib.module.ts
@@ -1,7 +1,79 @@
+class AppLoader extends HTMLElement {
+    //private shadow: ShadowRoot;
+
+    constructor() {
+        super();
+        //this.shadow = this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render = () => {
+        this.innerHTML = `
+            <style>
+            .loading {
+                width: 80dvw;
+                background-color: #FFF;
+                padding: 0.5rem;
+                border: none
+            }
+            .loading::backdrop {
+                background-color: rgba(35, 55, 60, 0.5);
+                backdrop-filter: blur(4px);
+            }
+
+            .loading-progress {
+            }
+
+            .loading-progress:after {
+                padding: 0.25rem;
+
+                display: block;
+                overflow: visible;
+                background-color: #c10b25;
+
+                width: var(--blazor-load-percentage, 0%);
+                transition: width 0.2s ease-in-out;
+                margin: 0 0 0 auto;
+
+                color: #FFF;
+                content: var(--blazor-load-percentage-text, "");
+                text-align: center;
+                font-weight: bold;
+                text-shadow: 1px 1px 0 #23373c, -1px 1px 0 #23373c, 1px -1px 0 #23373c, -1px -1px 0 #23373c;
+            }
+            </style>
+            <dialog class="loading">
+                <div class="loading-progress"></div>
+            </dialog> 
+        `;
+    }
+}
+
+
 export const beforeWebAssemblyStart = (options: any, extensions: any) => {
-    document.documentElement.style.setProperty('--blazor-loader-display', 'block');
+    const useLoader = !document.documentElement.getAttribute("no-loader");
+
+    if (!useLoader) {
+        return;
+    }
+
+    customElements.define('app-loader', AppLoader);
+
+    const appLoader = document.createElement('app-loader');
+    appLoader.setAttribute('id', '__app-loader');
+
+    document.documentElement.appendChild(appLoader);
+
+    appLoader.getElementsByTagName('dialog')[0].showModal()
 };
 
 export const afterWebAssemblyStarted = (blazor: any) => {
-    document.documentElement.style.setProperty('--blazor-loader-display', 'none');
+    const appLoader = document.getElementById('__app-loader');
+    if (appLoader) {
+        appLoader.getElementsByTagName('dialog')[0].close();
+        document.documentElement.removeChild(appLoader);
+    }
 }
