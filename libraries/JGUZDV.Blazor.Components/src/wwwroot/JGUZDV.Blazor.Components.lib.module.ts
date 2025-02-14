51f099de33b03b80327dd8593aefef78614f5464
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-14 11:11:05 +0100

    ENHANCEMENT: Add a small loader variant

--- libraries/JGUZDV.Blazor.Components/src/wwwroot/JGUZDV.Blazor.Components.lib.module.ts
+++ libraries/JGUZDV.Blazor.Components/src/wwwroot/JGUZDV.Blazor.Components.lib.module.ts
@@ -1,9 +1,6 @@
-class AppLoader extends HTMLElement {
-    //private shadow: ShadowRoot;
-
+class FullScreenAppLoader extends HTMLElement {
     constructor() {
         super();
-        //this.shadow = this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
@@ -22,9 +19,6 @@ class AppLoader extends HTMLElement {
             .loading::backdrop {
                 background-color: rgba(35, 55, 60, 0.5);
                 backdrop-filter: blur(4px);
-            }
-
-            .loading-progress {
             }
 
             .loading-progress:after {
@@ -52,28 +46,103 @@ class AppLoader extends HTMLElement {
     }
 }
 
+class PrerenderAppLoader extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render = () => {
+        this.innerHTML = `
+            <style>
+            @keyframes progress {
+                0% { --percentage: 0%; }
+                100% { --percentage: var(--blazor-load-percentage); }
+            }
+
+            @property --percentage {
+                syntax: '<percentage>';
+                inherits: true;
+                initial-value: 0%;
+            }
+
+            .preloaded-loader-progress {
+                  --percentage: var(--blazor-load-percentage);
+                  animation: progress 2s 0.5s forwards;
+                  width: 64px;
+                  aspect-ratio: 1;
+                  border-radius: 50%;
+                  position: absolute;
+                  bottom: 16px;
+                  right: 16px;
+                  overflow: hidden;
+                  display: grid;
+                  place-items: center;
+            }
+
+            .preloaded-loader-progress::before {
+                  content: "";
+                  position: absolute;
+                  width: 100%;
+                  height: 100%;
+                  background: conic-gradient(#c10b25 var(--percentage), #23373c 0);
+            }
+
+            .preloaded-loader-progress::after {
+                content: var(--blazor-load-percentage-text, "");
+                position: absolute;
+
+                color: #FFF;
+                text-align: center;
+                font-weight: bold;
+                text-shadow: 1px 1px 0 #23373c, -1px 1px 0 #23373c, 1px -1px 0 #23373c, -1px -1px 0 #23373c;
+
+                width: 75%;
+                background: #FFF;
+                aspect-ratio: 1;
+                align-content: center;
+                border-radius: 50%
+            }
+            </style>
+            <div role="progressbar" class="preloaded-loader-progress">
+            </div>
+        `;
+    }
+}
+
 
 export const beforeWebAssemblyStart = (options: any, extensions: any) => {
     const useLoader = !document.documentElement.getAttribute("no-loader");
+    const useFullscreenLoader = document.documentElement.getAttribute("loader-type") == 'fullscreen'
+        ? true : false;
 
     if (!useLoader) {
         return;
     }
 
-    customElements.define('app-loader', AppLoader);
+    customElements.define('app-loader', useFullscreenLoader ? FullScreenAppLoader : PrerenderAppLoader);
 
     const appLoader = document.createElement('app-loader');
     appLoader.setAttribute('id', '__app-loader');
 
     document.documentElement.appendChild(appLoader);
 
-    appLoader.getElementsByTagName('dialog')[0].showModal()
+    if (useFullscreenLoader) {
+        appLoader.setAttribute('data-fullscreen', 'true')
+        appLoader.getElementsByTagName('dialog')[0].showModal()
+    }
 };
 
 export const afterWebAssemblyStarted = (blazor: any) => {
     const appLoader = document.getElementById('__app-loader');
     if (appLoader) {
-        appLoader.getElementsByTagName('dialog')[0].close();
+        if (!!appLoader.getAttribute('data-fullscreen')) {
+            appLoader.getElementsByTagName('dialog')[0].close();
+        }
+
         document.documentElement.removeChild(appLoader);
     }
 }
