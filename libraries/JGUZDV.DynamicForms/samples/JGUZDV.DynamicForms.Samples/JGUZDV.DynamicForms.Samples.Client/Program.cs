848d2ad3b0e5d80492a5fd34091e68683789bc52
Aaron Puhl <aaron_puhl@web.de>
2025-02-17 10:24:08 +0100

    remove file service

--- libraries/JGUZDV.DynamicForms/samples/JGUZDV.DynamicForms.Samples/JGUZDV.DynamicForms.Samples.Client/Program.cs
+++ libraries/JGUZDV.DynamicForms/samples/JGUZDV.DynamicForms.Samples/JGUZDV.DynamicForms.Samples.Client/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using JGUZDV.L10n;
 using JGUZDV.DynamicForms.Model;
 using JGUZDV.Blazor.Components;
-using JGUZDV.DynamicForms.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSupportedCultures(["de","en"]);
@@ -10,7 +9,6 @@ builder.Services.AddSupportedCultures(["de","en"]);
 builder.Services.AddLocalization();
 builder.Services.AddScoped<ValueProvider>();
 builder.Services.AddToasts();
-builder.Services.AddScoped<IFileService, DefaultFileService>();
 builder.Services.AddHttpClient();
 
 await builder.Build().RunAsync();
