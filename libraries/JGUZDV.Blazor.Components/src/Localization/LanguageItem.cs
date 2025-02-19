150e3bf7613fcf8167e095d3f2d45adbf6f0ce1c
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-19 14:47:49 +0100

    Remove ILanguageService from here and package

--- libraries/JGUZDV.Blazor.Components/src/Localization/LanguageItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JGUZDV.Blazor.Components.Localization
-{
-    /// <summary>
-    /// Represents a language selection item
-    /// </summary>
-    /// <param name="Culture">The culture identifier, e.g. de-DE</param>
-    /// <param name="DisplayName">The (native) display name of the language.</param>
-    public record LanguageItem(string Culture, string DisplayName);
-}
