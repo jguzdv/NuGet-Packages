78a157f221b51d2a4e5ad6ce49477d65310a88f6
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-26 10:31:26 +0100

    Merge from main

--- libraries/JGUZDV.Blazor.Hosting/src/wwwroot/blazorStartup.js
+++
@@ -1,7 +0,0 @@
-﻿const applicationCulture = document.documentElement.getAttribute('blazor-culture') || 'de-DE';
-
-Blazor.start({
-    webAssembly: {
-        applicationCulture: applicationCulture
-    }
-});
