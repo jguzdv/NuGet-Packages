4962cca0336dd9c83d53652f4b8aa08fce414fc1
Thomas Ottenhus <glatzert@uni-mainz.de>
2025-02-28 10:24:58 +0100

    Blazor/components/static await (#166)
    * Enable prerendering with Streaming for Await and AwaitResult
    
    * CHORE: Slim down sample code
    
    * Allow StreamRendering for Awaits
    
    * Consolidate Versions
    
    * Bump version
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

--- libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
@@ -1,44 +1,4 @@
-using System.Security.Claims;
-
-using JGUZDV.AspNetCore.Components.Localization;
-using JGUZDV.Blazor.Components.Localization;
 using JGUZDV.Blazor.Hosting;
 
-using Microsoft.AspNetCore.Components.Authorization;
-
 var builder = JGUZDVWebAssemblyApplicationBuilder.CreateDefault(args);
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//builder.Services.AddSupportedCultures(new List<string>() { "de", "en" });
-
-//builder.Services.AddLocalization();
-//builder.Services.AddBrowserClientStore();
-
-//builder.Services.AddSingleton<AuthenticationStateProvider, SampleAuthenticationStateProvider>();
-//builder.Services.AddSingleton<ILanguageService, SampleLanguageService>();
-
-//builder.Services.AddCascadingAuthenticationState();
-//builder.Services.AddAuthorizationCore();
-
-
 await builder.Build().RunAsync();
-
-
-internal class SampleAuthenticationStateProvider : AuthenticationStateProvider
-{
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
-    {
-        return Task.FromResult(
-            new AuthenticationState(
-                new ClaimsPrincipal(
-                    new ClaimsIdentity(new List<Claim>
-                    {
-                        new("name", "Mustermann, Max")
-                    },
-                    "SampleAuth", "name", "role")
-                )
-            )
-        );
-    }
-}
--- libraries/JGUZDV.Blazor.Components/samples/Web/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web/Program.cs
@@ -1,6 +1,4 @@
 using JGUZDV.AspNetCore.Hosting;
-using JGUZDV.AspNetCore.Hosting.Localization;
-using JGUZDV.Blazor.Components.Localization;
 using JGUZDV.Blazor.Components.Samples.Web.Components;
 
 var builder = JGUZDVHostApplicationBuilder.CreateWebHost(args, JGUZDV.AspNetCore.Hosting.Components.BlazorInteractivityModes.WebAssembly);
