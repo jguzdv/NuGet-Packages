98ecc2cf4b8a3b6b22f78bdc8d2751e0cf477082
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-28 16:33:45 +0100

    Remove experiment

--- libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
@@ -1,11 +1,4 @@
-using System.Reflection.PortableExecutable;
-
 using JGUZDV.Blazor.Hosting;
 
 var builder = JGUZDVWebAssemblyApplicationBuilder.CreateDefault(args);
-
-builder.Services.AddAuthorizationCore();
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddAuthenticationStateDeserialization();
-
 await builder.Build().RunAsync();
