28db77dd96c8e12ad76ec619271d246243c6dd5f
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-28 16:18:03 +0100

    Move some bits around

--- libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
@@ -1,4 +1,11 @@
+using System.Reflection.PortableExecutable;
+
 using JGUZDV.Blazor.Hosting;
 
 var builder = JGUZDVWebAssemblyApplicationBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthenticationStateDeserialization();
+
 await builder.Build().RunAsync();
