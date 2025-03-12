cfc21e24de055b248b95f4172d9723e040e596d2
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-12 15:17:12 +0100

    Streamlining the auth-button

--- libraries/JGUZDV.Blazor.Components/samples/Web/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web/Program.cs
@@ -1,7 +1,16 @@
+using System.Security.Claims;
+
 using JGUZDV.AspNetCore.Hosting;
 using JGUZDV.Blazor.Components.Samples.Web.Components;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = JGUZDVHostApplicationBuilder.CreateWebHost(args, JGUZDV.AspNetCore.Hosting.Components.BlazorInteractivityModes.WebAssembly);
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x => x.ExpireTimeSpan = TimeSpan.FromMinutes(2));
 
 var app = builder.BuildAndConfigureBlazor<App>(
     additionalBlazorAssemblies: [
@@ -9,4 +18,20 @@ var app = builder.BuildAndConfigureBlazor<App>(
         typeof(JGUZDV.Blazor.Components.Samples._Imports).Assembly,
     ]);
 
+app.MapGet("_app/sign-in", (HttpContext context, string redirectUri) =>
+{
+    context.SignInAsync(
+        new ClaimsPrincipal(
+            new ClaimsIdentity(
+                [new("sub", "ABCDF"), new("name", "Gutenberg, Johannes")],
+                "Fake Login",
+                "sub",
+                "role"
+        )));
+
+    return Results.LocalRedirect(redirectUri);
+});
+
 app.Run();
+
+
