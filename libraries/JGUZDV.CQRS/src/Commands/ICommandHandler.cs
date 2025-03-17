11e3f95dad0228be1e7ddcf45d249fd50b0e06a4
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-17 16:44:29 +0100

    There are some error prone overloads, that hide you forgot to pass the principal. We're removing them.

--- libraries/JGUZDV.CQRS/src/Commands/ICommandHandler.cs
+++ libraries/JGUZDV.CQRS/src/Commands/ICommandHandler.cs
@@ -2,17 +2,14 @@
 
 namespace JGUZDV.CQRS.Commands;
 
+/// <summary>
+/// Describes the interface for a command handler.
+/// </summary>
 public interface ICommandHandler<in TCommand>
     where TCommand : ICommand
 {
-    Task<HandlerResult> ExecuteAsync(TCommand command)
-        => ExecuteAsync(command, ClaimsPrincipal.Current, default);
-
-    Task<HandlerResult> ExecuteAsync(TCommand command, ClaimsPrincipal principal)
-        => ExecuteAsync(command, principal, default);
-
-    Task<HandlerResult> ExecuteAsync(TCommand command, CancellationToken ct)
-        => ExecuteAsync(command, ClaimsPrincipal.Current, ct);
-
+    /// <summary>
+    /// Executes the command for the given principal.
+    /// </summary>
     Task<HandlerResult> ExecuteAsync(TCommand command, ClaimsPrincipal? principal, CancellationToken ct);
 }
