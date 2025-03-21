abc74abf07a5332ea381723284265c81d167c9a1
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-21 09:02:34 +0100

    Query base implementation

---
+++ libraries/JGUZDV.CQRS/src/Queries/QueryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace JGUZDV.CQRS.Queries;
+
+/// <summary>
+/// Base query implementation that already contains a result of T
+/// </summary>
+public abstract class QueryBase<T> : IQuery<T>
+{
+    /// <summary>
+    /// The result of the query.
+    /// </summary>
+    public QueryResult<T> Result { get; set; } = HandlerResult.Indeterminate();
+}
