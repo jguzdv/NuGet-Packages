c040076760891e0544bed2a6e75983f752caa1b0
Thomas Ottenhus <glatzert@uni-mainz.de>
2025-03-27 10:11:59 +0100

    Hosting/deduplication (#187)
    * Deduplicated some code
    
    * Add some sample file for configuration
    
    * Docs
    
    * Docs
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

--- libraries/JGUZDV.AspNetCore.Hosting/src/JGUZDVHostApplicationBuilderExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Hosting/src/JGUZDVHostApplicationBuilderExtensions.cs
@@ -123,7 +123,7 @@ public static class JGUZDVHostApplicationBuilderExtensions
         }
         else
         {
-            LogMessages.MissingConfig(logger, appBuilder.Environment.IsProduction() ? LogLevel.Information : LogLevel.Warning, "RequestLocalization", configSection);
+            LogMessages.DefaultRequestLocalizationConfig(logger, configSection);
         }
 
         appBuilder.Services.AddLocalization(opt =>
