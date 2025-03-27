5fcb16f5a37722c88da6f69a10bcdc124279bf95
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-27 12:54:43 +0100

    Fixing a flaw in the cropper, that occured with .NET 9

--- libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
+++ libraries/JGUZDV.Blazor.Components/samples/Web.Client/Program.cs
@@ -3,5 +3,7 @@ using JGUZDV.Blazor.Components;
 
 var builder = JGUZDVWebAssemblyApplicationBuilder.CreateDefault(args);
 
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
 var host = await builder.BuildAsync();
 await host.RunAsync();
