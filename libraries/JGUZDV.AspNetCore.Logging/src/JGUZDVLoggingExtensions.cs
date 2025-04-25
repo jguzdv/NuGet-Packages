61c7fd380598ba2f2bd5dbf38a8a591e2e8eb702
Ottenhus, Thomas <Ottenhus, Thomas>
2025-04-25 10:01:06 +0200

    Fix an anoyance

--- libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
@@ -36,6 +36,22 @@ public static class JGUZDVLoggingExtensions
         {
             logger?.LogError(FileLoggerSectionName + " section is missing in the configuration. " +
                 "JGUZDV Logging requires a file logging section in production.");
+
+
+            builder.Services.PostConfigure<FileLoggerOptions>(configureOptions =>
+            {
+                if (string.IsNullOrWhiteSpace(configureOptions.OutputDirectory))
+                {
+                    throw new ArgumentException("No property OutputDirectory found in config section Logging:File. " +
+                            "JGUZDV Logging needs a directory to store logfiles.");
+                }
+
+                // Add the application name to the output directory, so log files for
+                // different apps will be written to different directories.
+                configureOptions.OutputDirectory = Path.Combine(
+                    configureOptions.OutputDirectory,
+                    builder.Environment.ApplicationName);
+            });
         }
 
         // In Production we'll blindly add the JSON file logger.
@@ -52,22 +68,6 @@ public static class JGUZDVLoggingExtensions
                 builder.Logging.AddPlainTextFile();
             }
         }
-
-
-        builder.Services.PostConfigure<FileLoggerOptions>(configureOptions =>
-        {
-            if (string.IsNullOrWhiteSpace(configureOptions.OutputDirectory))
-            {
-                throw new ArgumentException("No property OutputDirectory found in config section Logging:File. " +
-                        "JGUZDV Logging needs a directory to store logfiles.");
-            }
-
-            // Add the application name to the output directory, so log files for
-            // different apps will be written to different directories.
-            configureOptions.OutputDirectory = Path.Combine(
-                configureOptions.OutputDirectory,
-                builder.Environment.ApplicationName);
-        });
     }
 }
 
