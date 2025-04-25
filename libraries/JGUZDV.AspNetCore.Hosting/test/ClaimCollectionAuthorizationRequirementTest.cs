fd7b411d7a289a63e2b638a18f2a9fed4023487d
Ottenhus, Thomas <Ottenhus, Thomas>
2025-04-25 09:05:05 +0200

    Change Authorization Policy building to be more verbose during output

---
+++ libraries/JGUZDV.AspNetCore.Hosting/test/ClaimCollectionAuthorizationRequirementTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+
+using JGUZDV.AspNetCore.Hosting.Authorization;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace JGUZDV.AspNetCore.Hosting.Tests
+{
+    public class ClaimCollectionAuthorizationRequirementTest
+    {
+        private ClaimsPrincipal CreateUser()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity([new Claim("role", "admin"),
+                new Claim("role", "user"),
+                new Claim("scope", "read"),
+                new Claim("scope", "write"),
+                new Claim("scope", "delete"),
+                new Claim("scp", "delete"),
+            ],
+            "FakeAuth", "name", "role"));
+        }
+
+        [Theory,
+            InlineData([new[] { "READ" }, true]),
+            InlineData([new[] { "READ", "invalid" }, true]),
+            InlineData([new[] { "invalid" }, false]),
+        ]
+        public async Task Test_ClaimCollectionAuthorizationRequirement_Any(string[] values, bool expectedResult)
+        {
+            var user = CreateUser();
+            var requirement = new ClaimCollectionAuthorizationRequirement(
+                ClaimCollectionAuthorizationRequirement.MatchType.Any,
+                "scope",
+                StringComparer.OrdinalIgnoreCase,
+                values);
+
+            var context = new AuthorizationHandlerContext(new[] { requirement }, CreateUser(), null);
+
+            await requirement.HandleAsync(context);
+            Assert.Equal(expectedResult, context.HasSucceeded);
+        }
+
+        [Theory,
+            InlineData([new[] { "READ" }, true]),
+            InlineData([new[] { "READ", "invalid" }, false]),
+            InlineData([new[] { "invalid" }, false]),
+        ]
+        public async Task Test_ClaimCollectionAuthorizationRequirement_All(string[] values, bool expectedResult)
+        {
+            var user = CreateUser();
+            var requirement = new ClaimCollectionAuthorizationRequirement(
+                ClaimCollectionAuthorizationRequirement.MatchType.All,
+                "scope",
+                StringComparer.OrdinalIgnoreCase,
+                values);
+            var context = new AuthorizationHandlerContext(new[] { requirement }, CreateUser(), null);
+            await requirement.HandleAsync(context);
+            Assert.Equal(expectedResult, context.HasSucceeded);
+        }
+    }
+}
--- libraries/JGUZDV.AspNetCore.Hosting/test/WebHostTests.cs
+++ libraries/JGUZDV.AspNetCore.Hosting/test/WebHostTests.cs
@@ -1,11 +1,24 @@
-﻿namespace JGUZDV.AspNetCore.Hosting.Tests;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JGUZDV.AspNetCore.Hosting.Tests;
 
 public class WebHostTests
 {
     [Fact]
-    public void Host_Will_Run_without_Configuration()
+    public void Host_Will_Run_with_Little_Configuration()
     {
-        var host = JGUZDVHostApplicationBuilder.CreateWebApi([]).BuildAndConfigureDefault();
+        var hostBuilder = JGUZDVHostApplicationBuilder.CreateWebApi([], ctx =>
+        {
+            var configs = new Dictionary<string, string?>
+            {
+                { "Logging:File:OutputDirectory", "."}
+            };
+
+            ctx.Configuration.AddInMemoryCollection(configs);
+        });
+        
+            
+        var host = hostBuilder.BuildAndConfigureDefault();
 
         _ = host.RunAsync();
         host.Lifetime.StopApplication();
