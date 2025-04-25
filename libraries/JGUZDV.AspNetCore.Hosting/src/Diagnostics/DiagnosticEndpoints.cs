1cbee4ef28028d95f55fd3a87ff02a49f30bca62
Ottenhus, Thomas <Ottenhus, Thomas>
2025-04-25 11:03:31 +0200

    Provide Diagnostics Endpoint in own method

---
+++ libraries/JGUZDV.AspNetCore.Hosting/src/Diagnostics/DiagnosticEndpoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace JGUZDV.AspNetCore.Hosting.Diagnostics
+{
+    /// <summary>
+    /// Extension methods for mapping authentication endpoints.
+    /// </summary>
+    public static class DiagnosticEndpointsExtensions
+    {
+        /// <summary>
+        /// Maps _diag endpoints to the route (_diag/user).
+        /// </summary>
+        public static IEndpointRouteBuilder MapDiagnostics(this IEndpointRouteBuilder route, string routePrefix = "_app/_diag")
+        {
+            var diagnostics = route.MapGroup(routePrefix)
+                .ExcludeFromDescription();
+
+            diagnostics.MapGet("user",
+                (ClaimsPrincipal currentUser) =>
+                {
+                    Results.Ok(new
+                    {
+                        Identities = currentUser.Identities.Select(identity => new
+                        {
+                            IsAuthenticated = identity.IsAuthenticated,
+                            IsAuthenticatedType = identity.AuthenticationType,
+                            Claims = currentUser.Claims.Select(c => new
+                            {
+                                c.Type,
+                                c.Value
+                            })
+                        })
+                    });
+                });
+
+            return route;
+        }
+    }
+}
