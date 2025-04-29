f54af29f530bfcd6ac863e425140296b61643f04
aaron-puhl <aaron_puhl@web.de>
2025-04-29 17:06:37 +0200

    ignore result (#217)

--- libraries/JGUZDV.CQRS/src/Queries/QueryBase.cs
+++ libraries/JGUZDV.CQRS/src/Queries/QueryBase.cs
@@ -1,4 +1,6 @@
-﻿namespace JGUZDV.CQRS.Queries;
+﻿using System.Text.Json.Serialization;
+
+namespace JGUZDV.CQRS.Queries;
 
 /// <summary>
 /// Base query implementation that already contains a result of T
@@ -8,5 +10,6 @@ public abstract record QueryBase<T> : IQuery<T>
     /// <summary>
     /// The result of the query.
     /// </summary>
+    [JsonIgnore]
     public QueryResult<T> Result { get; set; } = HandlerResult.Indeterminate();
 }
