3b3aa1ee1e744c23cc0b7330828f20da9812e0a4
Ottenhus, Thomas <Ottenhus, Thomas>
2025-04-30 14:29:57 +0200

    GITsch

--- libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Logging/src/JGUZDVLoggingExtensions.cs
@@ -1,5 +1,4 @@
-﻿using JGUZDV.AspNetCore.Logging;
-using JGUZDV.Extensions.Logging.File;
+﻿using JGUZDV.Extensions.Logging.File;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -28,27 +27,37 @@ public static class JGUZDVLoggingExtensions
 
     private static void AddFileLogging(WebApplicationBuilder builder, ILogger? logger)
     {
-        var isProduction = builder.Environment.IsProduction();
+        var isNotDevelopment = !builder.Environment.IsDevelopment();
         var hasFileSection = builder.Configuration.GetSection(FileLoggerSectionName).Exists();
 
         // We'll log a missing log file as an error in production.
-        if (isProduction && !hasFileSection)
+        if (isNotDevelopment && !hasFileSection)
         {
             logger?.LogCritical(FileLoggerSectionName + " section is missing in the configuration. " +
                 "JGUZDV Logging requires a file logging section in production.");
         }
 
-        // In Production we'll blindly add the JSON file logger.
-        if (isProduction)
+        if (hasFileSection)
         {
-            builder.Logging.AddJsonFile();
+            // In Production we'll blindly add the JSON file logger.
+            if (isNotDevelopment)
+            {
+                builder.Logging.AddJsonFile();
+            }
+            else
+            {
+                // In Development we'll add the plain text file logger.
+                builder.Logging.AddPlainTextFile();
+            }
+
 
             builder.Services.PostConfigure<FileLoggerOptions>(configureOptions =>
             {
                 if (string.IsNullOrWhiteSpace(configureOptions.OutputDirectory))
                 {
-                    throw new ArgumentException("No property OutputDirectory found in config section Logging:File. " +
-                            "JGUZDV Logging needs a directory to store logfiles.");
+                    throw new ArgumentException(
+                        "No property OutputDirectory found in config section Logging:File. " +
+                        "JGUZDV Logging needs a directory to store logfiles.");
                 }
 
                 // Add the application name to the output directory, so log files for
@@ -57,15 +66,6 @@ public static class JGUZDVLoggingExtensions
                     configureOptions.OutputDirectory,
                     builder.Environment.ApplicationName);
             });
-        }
-        else
-        {
-            // In Development we'll add the plain text file logger if the file logger section is present.
-            // Else it's pretty annoying to have a file logger in development.
-            if (hasFileSection)
-            {
-                builder.Logging.AddPlainTextFile();
-            }
         }
     }
 }
