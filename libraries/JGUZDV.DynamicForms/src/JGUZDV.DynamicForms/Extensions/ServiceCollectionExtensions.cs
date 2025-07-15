24e3314f5fbfaec5d7ce5927d933a93a61a20142
Aaron Puhl <aaropuhl@outlook.de>
2025-07-15 11:32:41 +0200

    handle warnings

--- libraries/JGUZDV.DynamicForms/src/JGUZDV.DynamicForms/Extensions/ServiceCollectionExtensions.cs
+++ libraries/JGUZDV.DynamicForms/src/JGUZDV.DynamicForms/Extensions/ServiceCollectionExtensions.cs
@@ -4,8 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace JGUZDV.DynamicForms.Extensions;
 
+/// <summary>
+/// Extensions to add dynamic forms to the service collection.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds dynamic forms services to the service collection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static DynamicFormsBuilder AddDynamicForms(this IServiceCollection services)
     {
         return new DynamicFormsBuilder(services);
