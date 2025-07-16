aac8c71dc1768286b4f37bc206980bf2de76a8a9
Thomas Ottenhus <glatzert@uni-mainz.de>
2025-07-16 16:18:24 +0200

    Extensions to detect, if a request should be answered with html (#248)
    * Add an extension determining, if a html page is requested
    
    * Bump version
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

---
+++ libraries/JGUZDV.AspNetCore.Hosting/src/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace JGUZDV.AspNetCore.Hosting.Extensions
+{
+    internal static class HttpRequestExtensions
+    {
+        public static bool IsHtmlPageRequest(this HttpRequest request)
+        {
+            // If a user requests the page, then he most likely wants a HTML page.
+            var secFetchUserHeader = request.Headers["Sec-Fetch-User"];
+            if (secFetchUserHeader.Count > 0 && secFetchUserHeader[0] == "?1")
+            {
+                return true;
+            }
+
+            // If the Sec-Fetch-Dest is "document", then it is likely a request for a HTML page.
+            var secFetchDestHeader = request.Headers["Sec-Fetch-Dest"];
+            if (secFetchDestHeader.Count > 0 && secFetchDestHeader[0] == "document")
+            {
+                return true;
+            }
+
+            // If the request has an Accept header that includes "text/html", then it is likely a request for a HTML page.
+            var acceptHeader = request.Headers["Accept"];
+            if (acceptHeader.Count > 0 && acceptHeader[0]!.Contains("text/html"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
