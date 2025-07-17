4b2fc2eaacfbd14209d63fdd78b9da4ca7f1243a
Ottenhus, Thomas <Ottenhus, Thomas>
2025-07-17 12:37:06 +0200

    make public and docs

--- libraries/JGUZDV.AspNetCore.Hosting/src/Extensions/HttpRequestExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Hosting/src/Extensions/HttpRequestExtensions.cs
@@ -2,8 +2,24 @@
 
 namespace JGUZDV.AspNetCore.Hosting.Extensions
 {
-    internal static class HttpRequestExtensions
+    /// <summary>
+    /// Provides extension methods for the <see cref="HttpRequest"/> class to simplify common request-related
+    /// operations.
+    /// </summary>
+    public static class HttpRequestExtensions
     {
+        /// <summary>
+        /// Determines whether the specified HTTP request is likely a request for an HTML page.
+        /// </summary>
+        /// <remarks>This method evaluates the request headers to determine if the request is intended for
+        /// an HTML page.  It checks the following conditions: <list type="bullet"> <item><description>The presence of
+        /// the "Sec-Fetch-User" header with a value of "?1".</description></item> <item><description>The presence of
+        /// the "Sec-Fetch-Dest" header with a value of "document".</description></item> <item><description>The "Accept"
+        /// header includes "text/html".</description></item> </list> If any of these conditions are met, the method
+        /// returns <see langword="true"/>.</remarks>
+        /// <param name="request">The HTTP request to evaluate. Cannot be <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if the request is likely a request for an HTML page; otherwise, <see
+        /// langword="false"/>.</returns>
         public static bool IsHtmlPageRequest(this HttpRequest request)
         {
             // If a user requests the page, then he most likely wants a HTML page.
