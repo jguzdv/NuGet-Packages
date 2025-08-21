78d97343d3e41d6ab423710f19a7c98c7647902b
Lunau, Marcus <lunaum@uni-mainz.de>
2025-08-21 11:17:46 +0200

    params aus summary entfernt

--- libraries/JGUZDV.L10n/src/L10nString.cs
+++ libraries/JGUZDV.L10n/src/L10nString.cs
@@ -56,7 +56,6 @@ public class L10nString : IEquatable<L10nString>, IEqualityComparer<L10nString>
     /// <summary>
     /// Initializes the instance from tuples which are defined as params. Each tuple contains a key and a value.
     /// </summary>
-    /// <param name="values"></param>
     public L10nString(params (string key, string value)[] values)
         :this()
     {
