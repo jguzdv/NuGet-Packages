5c22b7229dd17a4e98bb11ba3412f75993de9797
Ottenhus, Thomas <Ottenhus, Thomas>
2025-08-28 10:38:48 +0200

    Allow passing on language headers

---
+++ libraries/JGUZDV.AspNetCore.Hosting/src/Localization/HttpClientServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using JGUZDV.AspNetCore.Hosting.Localization;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions for httpclient builder
+    /// </summary>
+    public static class HttpClientServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a language header handler to the httpClientBuilder
+        /// </summary>
+        public static IHttpClientBuilder AddLocalizationHeaderHandler(
+            this IHttpClientBuilder httpClientBuilder)
+            => httpClientBuilder.AddHttpMessageHandler<LanguageHeaderHandler>();
+    }
+}
---
+++ libraries/JGUZDV.AspNetCore.Hosting/src/Localization/LanguageHeaderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace JGUZDV.AspNetCore.Hosting.Localization
+{
+    /// <summary>
+    /// Delegating handler, that adds the current culure and current ui culture to http headers.
+    /// </summary>
+    public class LanguageHeaderHandler : DelegatingHandler
+    {
+        /// <inheritdoc />
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var languages = request.Headers.AcceptLanguage.ToList();
+            languages.Insert(0, new(CultureInfo.CurrentCulture.Name, 1));
+            languages.Insert(0, new(CultureInfo.CurrentUICulture.Name, 1));
+
+            request.Headers.AcceptLanguage.Clear();
+            foreach (var headerValue in languages)
+            {
+                request.Headers.AcceptLanguage.Add(headerValue);
+            }
+
+            return base.Send(request, cancellationToken);
+        }
+    }
+}
