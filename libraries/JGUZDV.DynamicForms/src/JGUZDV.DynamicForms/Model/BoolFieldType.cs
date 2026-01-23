20d57919d3f18d222ab05f3fbf6ee292fb6ccb56
aaron-puhl <aaron_puhl@web.de>
2026-01-23 11:05:32 +0100

    Df/field types (#280)
    * add new field types
    
    * handle time conversion
    add primitive default handling for conversion
    
    * increase version
    
    ---------
    
    Co-authored-by: Aaron Puhl <aaropuhl@outlook.de>

---
+++ libraries/JGUZDV.DynamicForms/src/JGUZDV.DynamicForms/Model/BoolFieldType.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+using JGUZDV.L10n;
+
+namespace JGUZDV.DynamicForms.Model;
+
+/// <summary>
+/// Represents a field type for integer values.
+/// </summary>
+public record BoolFieldType : FieldType
+{
+    /// <summary>
+    /// Gets the CLR type of the field.
+    /// </summary>
+    [JsonIgnore]
+    public override Type ClrType => typeof(bool);
+
+    /// <summary>
+    /// Gets the display name of the field type.
+    /// </summary>
+    public override L10nString DisplayName => new L10nString()
+    {
+        ["de"] = "Boolesch",
+        ["en"] = "Boolean"
+    };
+
+    /// <summary>
+    /// Gets the input type of the field.
+    /// </summary>
+    [JsonIgnore]
+    public override string InputType => "checkbox";
+}
---
+++ libraries/JGUZDV.DynamicForms/src/JGUZDV.DynamicForms/Model/TimeFieldType.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using JGUZDV.L10n;
+
+namespace JGUZDV.DynamicForms.Model;
+
+/// <summary>
+/// Represents a field type for integer values.
+/// </summary>
+public record TimeFieldType : FieldType
+{
+    /// <summary>
+    /// Gets the CLR type of the field.
+    /// </summary>
+    [JsonIgnore]
+    public override Type ClrType => typeof(TimeOnly);
+
+    /// <summary>
+    /// Gets the display name of the field type.
+    /// </summary>
+    public override L10nString DisplayName => new L10nString()
+    {
+        ["de"] = "Uhrzeit",
+        ["en"] = "Time"
+    };
+
+    /// <summary>
+    /// Gets the input type of the field.
+    /// </summary>
+    [JsonIgnore]
+    public override string InputType => "time";
+
+    /// <inheritdoc/>
+    public override string ConvertFromValue(object value)
+    {
+        if (value is TimeOnly timeOnly)
+        {
+            return timeOnly.ToString("O");
+        }
+        throw new InvalidOperationException($"Invalid value type: {value.GetType().Name}. Expected TimeOnly.");
+    }
+
+    /// <inheritdoc/>
+    public override object ConvertToValue(string stringValue)
+    {
+        if (TimeOnly.TryParse(stringValue, out var dateOnly))
+        {
+            return dateOnly;
+        }
+        else
+        {
+            return JsonSerializer.Deserialize<TimeOnly>(stringValue, DynamicFormsConfiguration.JsonSerializerOptions);
+        }
+    }
+}
