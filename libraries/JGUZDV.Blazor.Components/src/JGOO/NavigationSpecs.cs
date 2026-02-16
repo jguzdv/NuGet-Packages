f945df7ec088b6c9209ed7048d278f3a2af1efa3
Aaron Puhl <aaropuhl@outlook.de>
2026-02-16 15:24:15 +0100

    refactor step 1

---
+++ libraries/JGUZDV.Blazor.Components/src/JGOO/NavigationSpecs.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace JGUZDV.Blazor.Components.JGOO;
+
+[Obsolete("Use AppHeader.NavItem or Sidebar.NavItem components instead. This will be removed in a future version.")]
+public class NavigationSpecs
+{
+    public RenderFragment? Icon { get; set; }
+    public string? NavUrl { get; set; }
+    public required string Name { get; set; }
+    public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+
+    public List<NavigationSpecs>? Children { get; set; }
+
+    public bool HasChildren => Children != null && Children.Any();
+}
