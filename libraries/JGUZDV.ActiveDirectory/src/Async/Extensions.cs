17ac77eee8a3b7f222e89db36cb1a777f26c7217
Ottenhus, Thomas <Ottenhus, Thomas>
2026-03-09 11:59:07 +0100

    Experiments

---
+++ libraries/JGUZDV.ActiveDirectory/src/Async/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.DirectoryServices.Protocols;
+
+namespace JGUZDV.ActiveDirectory.Async
+{
+    public static class LdapConnectionExtensions
+    {
+        extension(LdapConnection connection)
+        {
+            public async Task<T?> SendRequestAsync<T>(DirectoryRequest request, CancellationToken ct)
+                where T : DirectoryResponse
+            {
+                var asyncResult = connection.BeginSendRequest(request, PartialResultProcessing.NoPartialResultSupport, null, null);
+                using var ctr = ct.Register(() => connection.Abort(asyncResult));
+
+                return await Task.Factory.FromAsync(
+                    asyncResult,
+                    connection.EndSendRequest
+                ) as T;
+            }
+        }
+    }
+}
