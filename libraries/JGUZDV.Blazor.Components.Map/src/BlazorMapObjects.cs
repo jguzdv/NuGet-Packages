ffd8fbf2c8efd92051b6d44c3ce42930bef166d0
aysiebel <147617497+aysiebel@users.noreply.github.com>
2026-03-25 12:58:48 +0100

    Updated and Refactored Blazor Map (#304)
    * Moved BlazorMap from ZDV.Nuget-Package to here
    
    * changed name from test to tests
    
    * added ReadMe
    
    * updated JGUZDV.Blazor.Components to .net10, wrote xml documentation for map and removed variable static from it
    
    * some more refactorings
    
    * some more updates
    
    * cleanup
    
    * version

--- libraries/JGUZDV.Blazor.Components.Map/src/BlazorMapObjects.cs
+++ libraries/JGUZDV.Blazor.Components.Map/src/BlazorMapObjects.cs
@@ -8,16 +8,14 @@ namespace JGUZDV.Blazor.Components.Map
         public LngLat Center { get; set; }
         public int Zoom { get; set; }
         public List<LngLat>? MaxBounds { get; set; }
-        public bool IsStatic { get; set; }
         public string BaseLayerStyleUrl { get; set; }
         public IEnumerable<string>? AdditionalLayerStyleUrls { get; set; }
-        public Dictionary<string, FeatureCollection> AdditionalSourceData { get; set; }
+        public Dictionary<string, FeatureCollection>? AdditionalSourceData { get; set; }
         public string SpritePathPrefix { get; set; }
 
 
-        public BlazorMapObjects(bool isStatic, LngLat center, int zoom, string baseLayerStyleUrl, List<LngLat>? maxBounds, IEnumerable<string>? additionalLayerStyleUrls, Dictionary<string, FeatureCollection> additionalSourceData, string spritePathPrefix)
+        public BlazorMapObjects(LngLat center, int zoom, string baseLayerStyleUrl, List<LngLat>? maxBounds, IEnumerable<string>? additionalLayerStyleUrls, Dictionary<string, FeatureCollection>? additionalSourceData, string spritePathPrefix)
         {
-            IsStatic = isStatic;
             Center = center;
             Zoom = zoom;
             BaseLayerStyleUrl = baseLayerStyleUrl;
