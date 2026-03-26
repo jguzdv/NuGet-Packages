5574577f7401c690390555007b682af5f16434b1
Thomas Ottenhus <glatzert@uni-mainz.de>
2026-03-26 10:14:31 +0100

    Cqrs/more sugar (#306)
    * Add a function allowing a command to be run with a HttpContext without providing user or CancellationToken
    
    ---------
    
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

---
+++ libraries/JGUZDV.CQRS.AspNetCore/src/Http/CommandHandlerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using JGUZDV.CQRS.Commands;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace JGUZDV.CQRS.AspNetCore.Http
+{
+    public static class CommandHandlerExtensions
+    {
+        /// <summary>
+        /// Executes the given command and converts the result to IResult.
+        /// A common string localizer will be passed into ToHttpResult if available, so that the result can be localized.
+        /// </summary>
+        public static async Task<IResult> ExecuteCommandAsHttpResult<TCommand>(
+            this ICommandHandler<TCommand> commandHandler, TCommand command, HttpContext httpContext)
+            where TCommand : ICommand
+        {
+            var stringLocalizer = httpContext.RequestServices.GetService<IStringLocalizer>();
+            await commandHandler.ExecuteAsync(command, httpContext.User, httpContext.RequestAborted);
+            return HandlerResult.Indeterminate().ToHttpResult(stringLocalizer);
+        }
+    }
+}
