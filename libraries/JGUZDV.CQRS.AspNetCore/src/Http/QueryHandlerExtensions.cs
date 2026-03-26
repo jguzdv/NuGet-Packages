fed933e6d4240bea3bca12913258b63897799c58
Ottenhus, Thomas <Ottenhus, Thomas>
2026-03-26 09:56:35 +0100

    MORE sugar

--- libraries/JGUZDV.CQRS.AspNetCore/src/Http/QueryHandlerExtensions.cs
+++ libraries/JGUZDV.CQRS.AspNetCore/src/Http/QueryHandlerExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using JGUZDV.CQRS.Queries;
 
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 
 namespace JGUZDV.CQRS.AspNetCore.Http
 {
@@ -19,13 +21,16 @@ namespace JGUZDV.CQRS.AspNetCore.Http
 
         /// <summary>
         /// Executes the given query and converts the result to IResult.
+        /// A common string localizer will be passed into ToHttpResult if available, so that the result can be localized.
         /// </summary>
         public static async Task<IResult> ExecuteQueryAsHttpResult<TQuery, TValue>(
             this IQueryHandler<TQuery> queryHandler, IQuery<TValue> query, HttpContext httpContext)
             where TQuery : IQuery<TValue>
         {
+            var stringLocalizer = httpContext.RequestServices.GetService<IStringLocalizer>();
+
             await queryHandler.ExecuteQuery(query, httpContext.User, httpContext.RequestAborted);
-            return (query.Result ?? HandlerResult.Indeterminate()).ToHttpResult();
+            return (query.Result ?? HandlerResult.Indeterminate()).ToHttpResult(stringLocalizer);
         }
     }
 }
