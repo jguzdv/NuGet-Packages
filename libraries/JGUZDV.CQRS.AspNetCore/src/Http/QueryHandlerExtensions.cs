fd0bd6238f1eebd16341afd764afd8baaf41b300
Ottenhus, Thomas <Ottenhus, Thomas>
2026-03-26 09:47:12 +0100

    Add a convenience method that takes a HttpContext and picks user and RequestCancelled from that.

---
+++ libraries/JGUZDV.CQRS.AspNetCore/src/Http/QueryHandlerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JGUZDV.CQRS.Queries;
+
+using Microsoft.AspNetCore.Http;
+
+namespace JGUZDV.CQRS.AspNetCore.Http
+{
+    public static class QueryHandlerExtensions
+    {
+        /// <summary>
+        /// Executes the given query and returns the result.
+        /// </summary>
+        public static async Task<QueryResult<TValue>> ExecuteQuery<TQuery, TValue>(
+            this IQueryHandler<TQuery> queryHandler, IQuery<TValue> query, HttpContext httpContext)
+            where TQuery : IQuery<TValue>
+        {
+            await queryHandler.ExecuteQuery(query, httpContext.User, httpContext.RequestAborted);
+            return query.Result ?? HandlerResult.Indeterminate();
+        }
+    }
+}
