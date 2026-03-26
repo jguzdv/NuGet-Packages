179c5c25b5ef666a3d0a6e3855218f333976fcd1
Ottenhus, Thomas <Ottenhus, Thomas>
2026-03-26 10:12:28 +0100

    Forgot an importand sugar

---
+++ libraries/JGUZDV.CQRS.AspNetCore/src/Http/CommandHandlerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JGUZDV.CQRS.Commands;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace JGUZDV.CQRS.AspNetCore.Http
+{
+    public static class CommandHandlerExtensions
+    {
+        public static async Task<IResult> ExecuteCommandAsHttpResult<TCommand>(
+            this ICommandHandler<TCommand> commandHandler, TCommand command, HttpContext httpContext)
+            where TCommand : ICommand
+        {
+            var stringLocalizer = httpContext.RequestServices.GetService<IStringLocalizer>();
+            await commandHandler.ExecuteAsync(command, httpContext.User, httpContext.RequestAborted);
+            return HandlerResult.Indeterminate().ToHttpResult(stringLocalizer);
+        }
+    }
+}
