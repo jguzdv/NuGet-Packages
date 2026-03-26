770859dbaefdf769589e8b25793f900ab7c97aeb
Thomas Ottenhus <glatzert@uni-mainz.de>
2026-03-26 10:30:14 +0100

    Add another ExecuteCommand method for more brevity (#307)
    Co-authored-by: Ottenhus, Thomas <Ottenhus, Thomas>

--- libraries/JGUZDV.CQRS.AspNetCore/src/Http/CommandHandlerExtensions.cs
+++ libraries/JGUZDV.CQRS.AspNetCore/src/Http/CommandHandlerExtensions.cs
@@ -9,6 +9,16 @@ namespace JGUZDV.CQRS.AspNetCore.Http
     public static class CommandHandlerExtensions
     {
         /// <summary>
+        /// Executes the given command and returns the result using the User and CancellationToken from the HttpContext.
+        /// </summary>
+        public static async Task ExecuteCommand<TCommand>(
+            this ICommandHandler<TCommand> commandHandler, TCommand command, HttpContext httpContext)
+            where TCommand : ICommand
+        {
+            await commandHandler.ExecuteAsync(command, httpContext.User, httpContext.RequestAborted);
+        }
+
+        /// <summary>
         /// Executes the given command and converts the result to IResult.
         /// A common string localizer will be passed into ToHttpResult if available, so that the result can be localized.
         /// </summary>
