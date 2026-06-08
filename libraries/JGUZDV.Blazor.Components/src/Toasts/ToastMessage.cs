413da687d210a589e48e3d38dad0cf40b329c711
aysiebel <147617497+aysiebel@users.noreply.github.com>
2026-06-08 16:20:48 +0200

    Toast remove obsolete code (#319)
    * Toasts: Removed obsolete code

--- libraries/JGUZDV.Blazor.Components/src/Toasts/ToastMessage.cs
+++ libraries/JGUZDV.Blazor.Components/src/Toasts/ToastMessage.cs
@@ -5,21 +5,6 @@
     /// </summary>
     public class ToastMessage
     {
-        private readonly IToastService _toastService;
-
-        /// <summary>
-        /// Obsolete: Timeouts will not be supported anymore.
-        /// </summary>
-        [Obsolete("Timeouts will not be supported anymore.")]
-        public ToastMessage(ToastLevel toastLevel, string message, string? title, TimeSpan? timeout, IToastService toastService)
-        {
-            ToastLevel = toastLevel;
-            Message = message;
-            Title = title;
-
-            _toastService = toastService;
-        }
-
         /// <summary>
         /// Creates a new ToastMessage with the specified level, message, and title.
         /// </summary>
@@ -48,20 +33,8 @@
 
         /// <summary>
         /// The duration after which the toast will be automatically dismissed.
-        /// When set, the countdown pauses while the user hovers or focuses the toast (WCAG 2.2.1).
+        /// When set, the countdown pauses while the user hovers or focuses the toast.
         /// </summary>
         public TimeSpan? AutoDismissAfter { get; }
-
-
-        /// <summary>
-        /// Hides the current toast notification.
-        /// </summary>
-        /// <remarks>This method removes the toast notification from view. If the toast notification is
-        /// already hidden,  calling this method has no effect.</remarks>
-        [Obsolete("Hiding toasts via the message is not supported anymore.")]
-        public void Hide()
-        {
-            _toastService?.Hide(this);
-        }
     }
 }
--- libraries/JGUZDV.DynamicForms/samples/JGUZDV.DynamicForms.Samples/JGUZDV.DynamicForms.Samples.Client/Program.cs
+++ libraries/JGUZDV.DynamicForms/samples/JGUZDV.DynamicForms.Samples/JGUZDV.DynamicForms.Samples.Client/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using JGUZDV.L10n;
 using JGUZDV.DynamicForms.Extensions;
-using JGUZDV.Blazor.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSupportedCultures(["de","en"]);
 
 builder.Services.AddLocalization();
 
-builder.Services.AddToasts();
 builder.Services.AddDynamicForms();
 
 builder.Services.AddHttpClient();
