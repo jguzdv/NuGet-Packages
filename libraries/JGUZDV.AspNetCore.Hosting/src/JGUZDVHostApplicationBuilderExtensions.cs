14d5b25cfeb1faa384067580e6b9f5765f1bb563
Ottenhus, Thomas <Ottenhus, Thomas>
2026-09-10 11:49:53 +0200

    Test project .net version

--- libraries/JGUZDV.AspNetCore.Hosting/src/JGUZDVHostApplicationBuilderExtensions.cs
+++ libraries/JGUZDV.AspNetCore.Hosting/src/JGUZDVHostApplicationBuilderExtensions.cs
@@ -535,7 +535,7 @@ public static class JGUZDVHostApplicationBuilderExtensions
 
         if (scopes.Length == 0)
         {
-            //TODO: LogMessages.NoRequiredScopes(logger);
+            // TODO: LogMessages.NoRequiredScopes(logger);
         }
         else
         {
