b39e279185ab02cae6d427af69748b9310dfb082
Ottenhus, Thomas <Ottenhus, Thomas>
2026-09-21 09:59:49 +0200

    Email message data

---
+++ libraries/JGUZDV.Outbox/src/EMail/EmailMessageData.cs
@@ -0,0 +1,29 @@
+﻿namespace JGUZDV.Outbox.EMail
+{
+    /// <summary>
+    /// Represents an email message that is stored in the outbox for later sending.
+    /// </summary>
+    // TODO: We _might_ want to define the EmailMessage as T of the GenericOutboxMessage<T> instead of a separate class.
+    public class EmailMessageData
+    {
+        /// <summary>
+        /// Gets or sets the recipients of the email message.
+        /// </summary>
+        public required EmailRecipients EmailRecipients { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subject of the email message.
+        /// </summary>
+        public required string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the body content of the email message.
+        /// </summary>
+        public required string Body { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the body content is in HTML format.
+        /// </summary>
+        public bool IsHtmlBody { get; set; }
+    }
+}
---
+++ libraries/JGUZDV.Outbox/src/EMail/OutboxMessageExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace JGUZDV.Outbox.EMail;
+
+public static class OutboxMessageExtensions
+{
+    extension(OutboxMessage)
+    {
+        public static OutboxMessage CreatePlainTextEmail(
+            DateTimeOffset dueDate,
+
+            string subject,
+            string body,
+
+            List<string> to,
+            List<string>? cc = null,
+            List<string>? bcc = null
+        ) => CreateEmail(dueDate, subject, body, false, to, cc, bcc);
+
+        public static OutboxMessage CreateHtmlEmail(
+            DateTimeOffset dueDate,
+            string subject,
+            string htmlBody,
+            List<string> to,
+            List<string>? cc = null,
+            List<string>? bcc = null
+        ) => CreateEmail(dueDate, subject, htmlBody, true, to, cc, bcc);
+
+
+        public static OutboxMessage CreateEmail(
+            DateTimeOffset dueDate,
+            string subject,
+            string body,
+            bool hasHtmlBody,
+            List<string> to,
+            List<string>? cc = null,
+            List<string>? bcc = null
+        ) => OutboxMessage.Create(
+            dueDate, 
+            "Email", 
+            new EmailMessageData()
+            {
+                Subject = subject,
+                Body = body,
+                IsHtmlBody = hasHtmlBody,
+
+                EmailRecipients = new()
+                {
+                    To = to,
+                    Cc = cc,
+                    Bcc = bcc
+                }
+            });
+    }
+}
