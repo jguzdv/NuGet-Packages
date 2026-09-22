3138b9473de5d0adb3a54ca24e22250fc0742c70
Ottenhus, Thomas <Ottenhus, Thomas>
2026-09-22 11:07:11 +0200

    Casing dance step 2

---
+++ libraries/JGUZDV.Outbox/src/Email/EmailRecipients.cs
@@ -0,0 +1,26 @@
+﻿namespace JGUZDV.Outbox.Email
+{
+    /// <summary>
+    /// Represents the recipients of an email message, including To, Cc, and Bcc addresses.
+    /// </summary>
+    public class EmailRecipients
+    {
+        /// <summary>
+        /// The list of email addresses to which the email will be sent. 
+        /// This property is required and must contain at least one valid email address.
+        /// </summary>
+        public required List<string> To { get; set; }
+
+        /// <summary>
+        /// The list of email addresses to which the email will be sent as carbon copy (CC). 
+        /// This property is optional and can be null or empty if no CC recipients are specified.
+        /// </summary>
+        public List<string>? Cc { get; set; }
+
+        /// <summary>
+        /// The list of email addresses to which the email will be sent as blind carbon copy (BCC). 
+        /// This property is optional and can be null or empty if no BCC recipients are specified.
+        /// </summary>
+        public List<string>? Bcc { get; set; }
+    }
+}
---
+++ libraries/JGUZDV.Outbox/src/Email/OutboxSenderBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using JGUZDV.Outbox;
+using JGUZDV.Outbox.Email;
+using JGUZDV.Outbox.Email.Mailkit;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Provides extension methods for configuring Outbox with Mailkit
+/// </summary>
+public static class OutboxSenderBuilderExtensions
+{
+    extension(OutboxSenderBuilder builder)
+    {
+        /// <summary>
+        /// Configures the outbox sender to use Mailkit for sending emails.
+        /// </summary>
+        public OutboxSenderBuilder UseDefaultEmailSender(
+            Action<MailkitMessageSenderOptions> configureOptions
+        )
+        {
+            builder.UseMessageSender<MailkitEmailMessageSender, MailkitMessageSenderOptions>(configureOptions);
+            builder.UseMessageFactory<EmailMessageDataFactory, EmailMessageData>();
+
+            return builder;
+        }
+    }
+}
+
